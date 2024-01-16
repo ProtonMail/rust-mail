7a0105633e20301a30438cc51e794f10e4981388
Leander Beernaert <leander.beernaert@proton.ch>
2024-01-16 13:48:24 +0100

    fix: LocateBlockquoteResult member visibility

--- mail/proton-mail-message-detector/src/wasm.rs
+++ mail/proton-mail-message-detector/src/wasm.rs
@@ -3,8 +3,8 @@ use wasm_bindgen::prelude::*;
 // To build the wasm version
 // wasm-pack build -d ./web/pkg --target web --features="web"
 
-#[wasm_bindgen]
-pub struct LocateBlockquoteResult(String, String);
+#[wasm_bindgen(getter_with_clone)]
+pub struct LocateBlockquoteResult(pub String, pub String);
 #[wasm_bindgen]
 pub fn locate_blockquote(input: &str) -> LocateBlockquoteResult {
     let (before, after) = crate::locate_blockquote(input);
