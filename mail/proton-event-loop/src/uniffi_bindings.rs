f32e7a0d24ae24a596c17afceb78322470944914
Leander Beernaert <leander.beernaert@proton.ch>
2024-01-19 16:04:02 +0100

    fix: uniffi external callback limitation for event loop error handling

--- mail/proton-event-loop/src/uniffi_bindings.rs
+++ mail/proton-event-loop/src/uniffi_bindings.rs
@@ -1,14 +1,24 @@
-use crate::{LoopError, LoopErrorHandlerReply};
+/// Generate an uniffi error loop handler type.
+/// It's impossible with uniffi at the moment to use callbacks declared in other crates. Due to the generic nature
+/// of the event loop, this macro aims to help with generating the required boilerplate to set this up.
+/// ```
+/// use proton_event_loop::{gen_event_loop_error_handler, LoopError, LoopErrorHandler, LoopErrorHandlerReply};
+/// gen_event_loop_error_handler!(ExportedEventLoopName, WrapperName);
+///```
+#[macro_export]
+macro_rules! gen_event_loop_error_handler {
+    ($name:ident, $wrapper_name:ident) => {
+        #[uniffi::export(callback_interface)]
+        pub trait $name: Send + Sync {
+            fn on_error(&self, error: LoopError) -> LoopErrorHandlerReply;
+        }
 
-#[uniffi::export(callback_interface)]
-pub trait LoopErrorHandler: Send + Sync {
-    fn on_error(&self, error: LoopError) -> LoopErrorHandlerReply;
-}
+        pub struct $wrapper_name(pub Box<dyn $name>);
 
-pub struct UniffiLoopErrorHandler(pub Box<dyn LoopErrorHandler>);
-
-impl crate::LoopErrorHandler for UniffiLoopErrorHandler {
-    fn on_error(&self, error: LoopError) -> LoopErrorHandlerReply {
-        self.0.on_error(error)
-    }
+        impl LoopErrorHandler for $wrapper_name {
+            fn on_error(&self, error: LoopError) -> LoopErrorHandlerReply {
+                self.0.on_error(error)
+            }
+        }
+    };
 }
