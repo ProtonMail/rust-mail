25edfe4afd4b2c20cdcced067665e84393cad426
Leander Beernaert <leander.beernaert@proton.ch>
2024-01-22 10:31:36 +0100

    fix: Event Loop Uniffi Bindgen

--- mail/proton-event-loop/src/loop.rs
+++ mail/proton-event-loop/src/loop.rs
@@ -15,8 +15,6 @@ use std::sync::Arc;
 use std::time::Duration;
 
 #[derive(Debug, thiserror::Error)]
-#[cfg_attr(feature = "uniffi", derive(uniffi::Error))]
-#[cfg_attr(feature = "uniffi", uniffi(flat_error))]
 pub enum LoopError {
     #[error("Failed to read from store: {0}")]
     StoreRead(anyhow::Error),
@@ -32,7 +30,6 @@ const MAX_EVENTS_PER_POLL: usize = 50;
 
 /// Response returned by the `LoopErrorHandler` to control the behavior of the event loop after an error occurs.
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-#[cfg_attr(feature = "uniffi", derive(uniffi::Enum))]
 pub enum LoopErrorHandlerReply {
     /// Pause the loop execution until it is manually resumed.
     Pause,
