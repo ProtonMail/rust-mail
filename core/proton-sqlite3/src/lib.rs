e38b590b0c2b0ed8f931dc19abc30e4e5e42a1ec
Leander Beernaert <leander.beernaert@proton.ch>
2024-02-12 13:49:31 +0100

    chore: Move proton-sqlite3 this repo

---
+++ core/proton-sqlite3/src/lib.rs
@@ -0,0 +1,208 @@
+//! Proton Sqlite3 provides some utility classes to correctly initialize and manage sqlite3 connections. It ensures
+//! that all sqlite3 connections are initialized with WAL2 and Foreign Key support and provides a `SqliteConnectionPool`
+//! abstraction to re-use connections in multithreaded scenarios.
+//!
+//! ```
+//! use proton_sqlite3::SqliteConnectionPool;
+//!
+//! //let pool = SqliteConnectionPool::new("/tmp/sql.db");
+//! fn sql_example(pool: SqliteConnectionPool) {
+//!     std::thread::scope( |s| {
+//!         let pool_cloned = pool.clone();
+//!         s.spawn(move || {
+//!            let mut con = pool_cloned.acquire().expect("failed to acquire connection");
+//!            let _tx = con.transaction().expect("failed to create transaction");
+//!
+//!         });
+//!         let pool_cloned = pool.clone();
+//!         s.spawn(move || {
+//!            let con = pool_cloned.acquire().expect("failed to acquire connection");
+//!            let _ = con.execute("SELECT * FROM Users", ()).expect_err("This query fails");
+//!         });
+//!     });
+//! }
+//! ```
+
+use log::error;
+use std::ops::{Deref, DerefMut};
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
+
+// re-export;
+pub use rusqlite;
+use rusqlite::{Connection, Transaction};
+
+pub const DEFAULT_OPEN_CONNECTION_LIMIT: usize = 8;
+
+/// A connection borrowed from a pool. On drop will be returned to the pool it was acquired from.
+pub struct SqliteConnection {
+    pool: Arc<ConnectionPoolInner>,
+    // Unfortunately we can't transfer this resource on drop, so the only option we have is to wrap it with option.
+    conn: Option<Connection>,
+}
+
+impl SqliteConnection {
+    /// Convenience transaction wrapper. Creates a new transaction an if the supplied closure does not return an error,
+    /// the transaction is committed. On Error, the transaction is rolled back.
+    pub fn tx<E: From<rusqlite::Error>, T, F: FnMut(&Transaction) -> Result<T, E>>(
+        &mut self,
+        mut closure: F,
+    ) -> Result<T, E> {
+        // Default behavior is to roll back the transaction on drop.
+        let mut tx = self.deref_mut().transaction()?;
+        let value = (closure)(&mut tx)?;
+
+        tx.commit()?;
+
+        Ok(value)
+    }
+}
+impl Drop for SqliteConnection {
+    fn drop(&mut self) {
+        self.pool.release(self.conn.take().unwrap());
+    }
+}
+
+impl Deref for SqliteConnection {
+    type Target = Connection;
+
+    fn deref(&self) -> &Self::Target {
+        self.conn.as_ref().expect("Should always be valid")
+    }
+}
+
+impl DerefMut for SqliteConnection {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.conn.as_mut().expect("Should always be valid")
+    }
+}
+
+/// Manages a pool of Sqlite3 connections and maintains up to a user defined connections open for re-use.
+#[derive(Clone)]
+pub struct SqliteConnectionPool {
+    inner: Arc<ConnectionPoolInner>,
+}
+struct ConnectionPoolInner {
+    lock: Mutex<Vec<Connection>>,
+    max_open_connections: usize,
+    path: PathBuf,
+}
+
+impl SqliteConnectionPool {
+    pub fn new(path: impl Into<PathBuf>) -> Self {
+        Self::with_open_connections_limit(path, DEFAULT_OPEN_CONNECTION_LIMIT)
+    }
+
+    pub fn with_open_connections_limit(path: impl Into<PathBuf>, limit: usize) -> Self {
+        Self {
+            inner: Arc::new(ConnectionPoolInner {
+                path: path.into(),
+                lock: Mutex::new(Vec::with_capacity(limit)),
+                max_open_connections: limit,
+            }),
+        }
+    }
+
+    pub fn acquire(&self) -> rusqlite::Result<SqliteConnection> {
+        self.inner
+            .get_or_create_connection()
+            .map(|c| SqliteConnection {
+                pool: self.inner.clone(),
+                conn: Some(c),
+            })
+    }
+
+    /// Close all connections in the pool. If a connection can't be closed, it will be put back into the pool
+    /// and the error is returned.
+    pub fn close_all(&self) -> rusqlite::Result<()> {
+        self.inner.close_all()
+    }
+}
+
+impl ConnectionPoolInner {
+    fn get_or_create_connection(&self) -> rusqlite::Result<Connection> {
+        {
+            let mut accessor = self.lock.lock().expect("lock poisoning");
+            if let Some(c) = accessor.pop() {
+                return Ok(c);
+            }
+        }
+
+        self.new_connection()
+    }
+
+    fn release(&self, conn: Connection) {
+        {
+            let mut accessor = self.lock.lock().expect("lock poisoning");
+            if accessor.len() < self.max_open_connections {
+                accessor.push(conn);
+                return;
+            }
+        }
+
+        if let Err((_, e)) = conn.close() {
+            error!("Failed to close connection: {e}");
+        }
+    }
+
+    fn new_connection(&self) -> rusqlite::Result<Connection> {
+        let conn = Connection::open(&self.path)?;
+        conn.execute("PRAGMA foreign_keys = ON;", ())?;
+        conn.execute("PRAGMA synchronous=NORMAL;", ())?;
+        conn.execute("PRAGMA journal=WAL2;", ())?;
+
+        Ok(conn)
+    }
+
+    fn close_all(&self) -> rusqlite::Result<()> {
+        let mut accessor = self.lock.lock().expect("lock poisoning");
+        while let Some(c) = accessor.pop() {
+            if let Err((c, e)) = c.close() {
+                accessor.push(c);
+                return Err(e);
+            }
+        }
+        Ok(())
+    }
+
+    #[cfg(test)]
+    fn get_open_connection_count(&self) -> usize {
+        self.lock.lock().expect("lock poisoning").len()
+    }
+}
+
+#[cfg(test)]
+fn new_test_dir() -> tempdir::TempDir {
+    tempdir::TempDir::new("proton-sqlite3").expect("Failed to create tmp dir")
+}
+#[test]
+fn test_connection_pool() {
+    let dir = new_test_dir();
+    const CONN_LIMIT: usize = 2;
+    let pool =
+        SqliteConnectionPool::with_open_connections_limit(dir.path().join("sql.db"), CONN_LIMIT);
+    assert_eq!(pool.inner.get_open_connection_count(), 0);
+
+    // Acquire 2 connections and then release them to the pool.
+    {
+        let _c1 = pool.acquire().expect("failed to acquire");
+        assert_eq!(pool.inner.get_open_connection_count(), 0);
+        let _c2 = pool.acquire().expect("failed to acquire");
+        assert_eq!(pool.inner.get_open_connection_count(), 0);
+    }
+    assert_eq!(pool.inner.get_open_connection_count(), 2);
+
+    // Acquire 3 connection, 2 should come from the pool and only 2
+    {
+        let _c1 = pool.acquire().expect("failed to acquire");
+        assert_eq!(pool.inner.get_open_connection_count(), 1);
+        let _c2 = pool.acquire().expect("failed to acquire");
+        assert_eq!(pool.inner.get_open_connection_count(), 0);
+        let _c3 = pool.acquire().expect("failed to acquire");
+        assert_eq!(pool.inner.get_open_connection_count(), 0);
+    }
+    assert_eq!(pool.inner.get_open_connection_count(), 2);
+
+    pool.close_all().expect("failed to close all connections");
+    assert_eq!(pool.inner.get_open_connection_count(), 0);
+}
---
+++ core/proton-sqlite3/tests/main.rs
@@ -0,0 +1,156 @@
+use proton_sqlite3::rusqlite::Result;
+use proton_sqlite3::SqliteConnectionPool;
+use rand::prelude::*;
+use rusqlite::hooks::Action;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Person2 {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+    data2: Option<Vec<u8>>,
+}
+
+fn run_tasks(pool: SqliteConnectionPool, count: usize) -> Result<()> {
+    let mut conn = pool.acquire()?;
+    let mut rng = thread_rng();
+
+    let thread_id = std::thread::current().id();
+    conn.update_hook(Some(move |action, db:&str, table:&str, row_id| {
+        match action {
+            Action::UNKNOWN => {
+                println!("[{:?}][{db}:{table}] Unknown action", thread_id);
+            },
+            Action::SQLITE_DELETE => {
+                println!("[{:?}][{db}:{table}] delete {row_id}", thread_id);
+            },
+            Action::SQLITE_INSERT => {
+                println!("[{:?}][{db}:{table}] insert {row_id}", thread_id);
+            }
+            Action::SQLITE_UPDATE => {
+                println!("[{:?}][{db}:{table}] update {row_id}", thread_id);
+            }
+            _ => {
+                println!("[{:?}][{db}:{table}] Other???", thread_id);
+            }
+        };
+    }));
+
+    for _ in 0..count {
+        match rng.gen::<u32>() % 3 {
+            0 => {
+                let mut stmt = conn.prepare("SELECT person.id, person.name, person.data , person_map.data2 FROM person JOIN person_map ON person.id=person_map.person")?;
+                let person_iter = stmt.query_map([], |row| {
+                    Ok(Person2 {
+                        id: row.get(0)?,
+                        name: row.get(1)?,
+                        data: row.get(2)?,
+                        data2: row.get(3)?,
+                    })
+                })?;
+                let _count = person_iter.into_iter().count();
+            }
+            1 => {
+                conn.tx(|tx| -> Result<()> {
+                    let mut nums: Vec<u8> = (1..20).collect();
+                    nums.shuffle(&mut rng);
+                    let me = Person {
+                        id: 0,
+                        name: format!("{:?}", nums),
+                        data: Some(nums.clone()),
+                    };
+
+                    let id: i32 = tx.query_row(
+                        "INSERT INTO person (name, data) VALUES (?1, ?2) RETURNING `id`",
+                        (&me.name, &me.data),
+                        |row| row.get(0),
+                    )?;
+
+                    nums.shuffle(&mut rng);
+
+                    tx.execute(
+                        "INSERT INTO person_map (person, data2) VALUES (?1, ?2)",
+                        (id, format!("{:?}", nums)),
+                    )?;
+                    Ok(())
+                })?;
+            }
+
+            2 => {
+                conn.tx(|tx| -> Result<()> {
+                    tx.execute(
+                        "DELETE FROM person WHERE id=(SELECT id FROM person ORDER BY RANDOM() LIMIT 1)",
+                        (),
+                    )?;
+                    Ok(())
+                })?;
+            }
+            _ => {
+                panic!("Unhandled");
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[test]
+fn run_random_command_test() {
+    // Simple execution test that spawns a couple of threads that executes a create, select or delete
+    // command at random.
+    let dir = tempdir::TempDir::new("sqlite3_test").expect("failed to create temp dir");
+    let db_path = dir.path().join("sqlite.db");
+
+    let connection_pool = SqliteConnectionPool::new(db_path);
+
+    {
+        // Create db tables.
+        let conn = connection_pool
+            .acquire()
+            .expect("Failed to acquire connection");
+
+        conn.execute(
+            "CREATE TABLE person (
+            id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            data BLOB
+        )",
+            (), // empty list of parameters.
+        )
+        .expect("failed to run create query");
+
+        conn.execute(
+            "CREATE TABLE person_map (
+            person INTEGER,
+            data2 TEXT NOT NULL,
+            CONSTRAINT `person_ref` FOREIGN KEY (`person`) REFERENCES `person` (`id`) ON DELETE SET NULL
+        )",
+            (), // empty list of parameters.
+        ).expect("failed to run create query");
+    }
+
+    let mut handles = Vec::new();
+    for _ in 0..100{
+        let pool = connection_pool.clone();
+        handles.push(std::thread::spawn(move || {
+            run_tasks(pool, 100).expect("Failed to execute");
+        }));
+    }
+
+    for h in handles {
+        h.join().expect("thread panicked");
+    }
+
+    connection_pool
+        .close_all()
+        .expect("Failed to close all connections");
+}
