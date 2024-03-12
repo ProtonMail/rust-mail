1e2a5dbd4367ec92811930cd4692011a293ed4f9
Leander Beernaert <leander.beernaert@proton.ch>
2024-03-12 08:54:39 +0100

    fix: Uniffi code gen for integer custom types.
    Due to https://github.com/mozilla/uniffi-rs/issues/2025 it is not
    possible to use integer custom types on functions/methods exported by
    uniffi.

--- mail/proton-mail-uniffi/src/mail/type_forwarding.rs
+++ mail/proton-mail-uniffi/src/mail/type_forwarding.rs
@@ -1,6 +1,5 @@
 use proton_mail_common::proton_api_mail as papi_mail;
 use proton_mail_common::proton_api_mail::proton_api_core as papi_core;
-use proton_mail_common::proton_mail_db as pm_db;
 
 // Required due to https://github.com/mozilla/uniffi-rs/issues/1988.
 
@@ -21,5 +20,3 @@ uniffi::ffi_converter_forward!(
     papi_mail::UniFfiTag,
     crate::UniFfiTag
 );
-
-uniffi::ffi_converter_forward!(pm_db::LocalLabelId, pm_db::UniFfiTag, crate::UniFfiTag);
