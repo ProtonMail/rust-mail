cc4ded0a0d9183df177379dfc30af55b9303a828
Leander Beernaert <leander.beernaert@proton.ch>
2024-03-12 14:59:44 +0100

    refactor: Make event loop poll and context initialization async

--- mail/proton-mail-commom/src/user_context/initialization.rs
+++ mail/proton-mail-commom/src/user_context/initialization.rs
@@ -26,58 +26,66 @@ impl MailUserContext {
     ) {
         let ctx = self.clone();
         self.mail_context().async_runtime().spawn(async move {
-            trace!("Syncing User settings");
-            cb.on_stage(MailUserContextLoadingStage::User);
-            if let Err(e) = ctx.inner.user_context.sync_user_and_settings().await {
-                error!("Failed to sync user settings: {e}");
-                cb.on_stage_err(MailUserContextLoadingStage::User, e.into());
-                return;
+            if let Err((stage, err)) = ctx.initialize_async(label_id, cb.as_ref()).await {
+                cb.on_stage_err(stage, err);
             }
-
-            trace!("Syncing event id");
-            cb.on_stage(MailUserContextLoadingStage::Events);
-            if let Err(e) = ctx.inner.event_loop.initialize(&ctx, &ctx).await {
-                error!("Failed to sync event id:{e}");
-                cb.on_stage_err(MailUserContextLoadingStage::Events, e.into());
-                return;
-            }
-
-            trace!("Syncing Addresses");
-            cb.on_stage(MailUserContextLoadingStage::Addresses);
-            if let Err(e) = ctx.sync_addresses().await {
-                error!("Failed to sync addresses :{e}");
-                cb.on_stage_err(MailUserContextLoadingStage::Addresses, e);
-                return;
-            }
-
-            // load labels
-            trace!("Syncing labels");
-            cb.on_stage(MailUserContextLoadingStage::Labels);
-            if let Err(e) = ctx.sync_labels().await {
-                error!("Failed to sync labels: {e}");
-                cb.on_stage_err(MailUserContextLoadingStage::Labels, e);
-                return;
-            }
-
-            // load conversation counters
-            trace!("Syncing conversation and message counts");
-            cb.on_stage(MailUserContextLoadingStage::Counters);
-            if let Err(e) = ctx.sync_conversation_and_message_counts().await {
-                error!("Failed to sync conversation and messages counter: {e}");
-                cb.on_stage_err(MailUserContextLoadingStage::Counters, e);
-            }
-
-            // load inbox conversations
-            trace!("Syncing Inbox conversations");
-            cb.on_stage(MailUserContextLoadingStage::Conversation);
-            if let Err(e) = ctx.sync_first_conversation_page(label_id, 50).await {
-                error!("Failed to sync Inbox conversation: {e}");
-                cb.on_stage_err(MailUserContextLoadingStage::Conversation, e);
-                return;
-            }
-
-            trace!("Syncing Complete");
-            cb.on_stage(MailUserContextLoadingStage::Finished);
         });
+    }
+
+    #[tracing::instrument(level = Level::DEBUG, skip(self, cb), fields(user_id=?self.user_id()))]
+    pub async fn initialize_async(
+        &self,
+        label_id: LabelId,
+        cb: &dyn MailUserContextInitializationCallback,
+    ) -> Result<(), (MailUserContextLoadingStage, MailContextError)> {
+        let ctx = self;
+        trace!("Syncing User settings");
+        cb.on_stage(MailUserContextLoadingStage::User);
+        if let Err(e) = ctx.inner.user_context.sync_user_and_settings().await {
+            error!("Failed to sync user settings: {e}");
+            return Err((MailUserContextLoadingStage::User, e.into()));
+        }
+
+        trace!("Syncing event id");
+        cb.on_stage(MailUserContextLoadingStage::Events);
+        if let Err(e) = ctx.inner.event_loop.initialize(ctx, ctx).await {
+            error!("Failed to sync event id:{e}");
+            return Err((MailUserContextLoadingStage::Events, e.into()));
+        }
+
+        trace!("Syncing Addresses");
+        cb.on_stage(MailUserContextLoadingStage::Addresses);
+        if let Err(e) = ctx.sync_addresses().await {
+            error!("Failed to sync addresses :{e}");
+            return Err((MailUserContextLoadingStage::Addresses, e));
+        }
+
+        // load labels
+        trace!("Syncing labels");
+        cb.on_stage(MailUserContextLoadingStage::Labels);
+        if let Err(e) = ctx.sync_labels().await {
+            error!("Failed to sync labels: {e}");
+            return Err((MailUserContextLoadingStage::Labels, e));
+        }
+
+        // load conversation counters
+        trace!("Syncing conversation and message counts");
+        cb.on_stage(MailUserContextLoadingStage::Counters);
+        if let Err(e) = ctx.sync_conversation_and_message_counts().await {
+            error!("Failed to sync conversation and messages counter: {e}");
+            return Err((MailUserContextLoadingStage::Counters, e));
+        }
+
+        // load inbox conversations
+        trace!("Syncing Inbox conversations");
+        cb.on_stage(MailUserContextLoadingStage::Conversation);
+        if let Err(e) = ctx.sync_first_conversation_page(label_id, 50).await {
+            error!("Failed to sync Inbox conversation: {e}");
+            return Err((MailUserContextLoadingStage::Conversation, e));
+        }
+
+        trace!("Syncing Complete");
+        cb.on_stage(MailUserContextLoadingStage::Finished);
+        Ok(())
     }
 }
