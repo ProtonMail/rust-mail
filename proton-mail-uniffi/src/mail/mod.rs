868d91403b6a1bd6cc4f5171702e1931edc0f9b6
Leander Beernaert <leander.beernaert@proton.ch>
2024-03-13 07:28:16 +0100

    chore: Expose logout to `MailUserContext`

--- proton-mail-uniffi/src/mail/mod.rs
+++ proton-mail-uniffi/src/mail/mod.rs
@@ -7,4 +7,10 @@ mod user_context;
 
 pub use context::*;
 pub use login::*;
+use proton_mail_common::exports::anyhow::anyhow;
 pub use user_context::*;
+
+#[inline]
+fn map_task_join_error(e: Box<dyn std::error::Error>) -> MailContextError {
+    MailContextError::Other(anyhow!("Failed to join task: {e}"))
+}
