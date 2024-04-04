15af7cd75e53af65aefbf2670074b2cb78e51e3a
Leander Beernaert <leander.beernaert@proton.ch>
2024-04-04 15:38:22 +0200

    feat(ET-92): Label and Unlabel actions

--- mail/proton-mail-common/src/actions/mod.rs
+++ mail/proton-mail-common/src/actions/mod.rs
@@ -1,14 +1,18 @@
 mod delete_conversations;
 mod event_loop;
+mod label_conversations;
 mod mark_conversations_read;
 mod mark_conversations_unread;
+mod unlabel_conversations;
 
 use crate::WeakMailUserContext;
 pub use delete_conversations::*;
 pub use event_loop::*;
+pub use label_conversations::*;
 pub use mark_conversations_read::*;
 pub use mark_conversations_unread::*;
 use proton_action_queue::ActionFactory;
+pub use unlabel_conversations::*;
 
 pub(crate) fn new_action_factory(mail_user_context: WeakMailUserContext) -> ActionFactory {
     let mut factory = ActionFactory::new();
@@ -26,6 +30,16 @@ pub(crate) fn new_action_factory(mail_user_context: WeakMailUserContext) -> Acti
         .expect(ERR_MSG);
     factory
         .register(Box::new(MarkConversationsUnreadActionFactory::new(
+            mail_user_context.clone(),
+        )))
+        .expect(ERR_MSG);
+    factory
+        .register(Box::new(LabelConversationsActionFactory::new(
+            mail_user_context.clone(),
+        )))
+        .expect(ERR_MSG);
+    factory
+        .register(Box::new(UnlabelConversationsActionFactory::new(
             mail_user_context.clone(),
         )))
         .expect(ERR_MSG);
