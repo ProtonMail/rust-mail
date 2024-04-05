193781eae54933cb68e9689da7a295b032ad4af0
Dan Williams <dan.williams@proton.ch>
2024-04-05 14:14:16 +0100

    test: Changed integration test to use async wrapper

--- proton-api-mail/tests/client.rs
+++ proton-api-mail/tests/client.rs
@@ -13,17 +13,20 @@ use wiremock::{
 mod basic {
     use super::*;
 
-    #[tokio::test]
-    async fn ping_mock_server() {
-        let mock_server = MockServer::start().await;
-        Mock::given(method("GET"))
-            .and(path("/api/ping"))
-            .respond_with(ResponseTemplate::new(200))
-            .expect(1)
-            .mount(&mock_server)
-            .await;
-        let (status, _, _, body) = request(format!("{}/api/ping", mock_server.uri())).await;
-        assert_eq!(status, StatusCode::OK);
-        assert_eq!(body.as_ref(), b"");
+    #[test]
+    fn ping_mock_server() {
+        let runtime = proton_async::runtime::LocalRuntime::new().expect("failed to create runtime");
+        runtime.block_on(async {
+            let mock_server = MockServer::start().await;
+            Mock::given(method("GET"))
+                .and(path("/api/ping"))
+                .respond_with(ResponseTemplate::new(200))
+                .expect(1)
+                .mount(&mock_server)
+                .await;
+            let (status, _, _, body) = request(format!("{}/api/ping", mock_server.uri())).await;
+            assert_eq!(status, StatusCode::OK);
+            assert_eq!(body.as_ref(), b"");
+        });
     }
 }
