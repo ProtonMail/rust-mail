3806538f4c9bb5dd2619a64f850f6e083d405601
Dan Williams <dan.williams@proton.ch>
2024-04-06 09:21:57 +0100

    test: Removed integration tests from proton-mail-common
      - Removed the integration test foundation from proton-mail-common, as
        apparently they don't belong there.
    
      - Moved the common integration test code to proton-api-mail.
    
      - Refined the integration test approach in proton-api-mail.

--- proton-api-mail/tests/client.rs
+++ proton-api-mail/tests/client.rs
@@ -1,8 +1,8 @@
 #[allow(unused)]
-#[path = "../../proton-mail-common/tests/common/mod.rs"]
 mod common;
 
 use crate::common::request;
+use proton_async::runtime::LocalRuntime;
 use reqwest::StatusCode;
 use wiremock::{
     matchers::{method, path},
@@ -15,18 +15,25 @@ mod basic {
 
     #[test]
     fn ping_mock_server() {
-        let runtime = proton_async::runtime::LocalRuntime::new().expect("failed to create runtime");
-        runtime.block_on(async {
+        let runtime = LocalRuntime::new().expect("failed to create runtime");
+        // Set up mock server with expectations and mock responses
+        let mock_server = runtime.block_on(async {
             let mock_server = MockServer::start().await;
+            // Simple response check to ensure the setup is working — this endpoint only
+            // exists on the mock server
             Mock::given(method("GET"))
                 .and(path("/api/ping"))
                 .respond_with(ResponseTemplate::new(200))
                 .expect(1)
                 .mount(&mock_server)
                 .await;
-            let (status, _, _, body) = request(format!("{}/api/ping", mock_server.uri())).await;
-            assert_eq!(status, StatusCode::OK);
-            assert_eq!(body.as_ref(), b"");
+            mock_server
         });
+        // Make a request to the mock server
+        let (status, _, _, body) =
+            runtime.block_on(async { request(format!("{}/api/ping", mock_server.uri())).await });
+        // Assert the response is as expected
+        assert_eq!(status, StatusCode::OK);
+        assert_eq!(body.as_ref(), b"");
     }
 }
