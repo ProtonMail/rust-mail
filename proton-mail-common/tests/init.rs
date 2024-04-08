ac1a8f80860b7fc332a6eb6cddf49975a3ae8d05
Dan Williams <dan.williams@proton.ch>
2024-04-08 18:03:27 +0100

    test: Streamlined test code

--- proton-mail-common/tests/init.rs
+++ proton-mail-common/tests/init.rs
@@ -1,15 +1,18 @@
+#[allow(unused)]
 mod common;
 
+use common::init::{setup_user, NullCallback, Params as TestParams};
+use common::TestContext;
 use proton_api_mail::domain::LabelId;
 
 #[test]
 fn test_init_after_login() {
-    let ctx = common::TestContext::new();
+    let ctx = TestContext::new();
     let user_ctx = ctx.user_context();
+    let init_params = TestParams::default_basic();
+    let cb = NullCallback {};
     ctx.async_runtime().block_on(async {
-        let init_params = common::init::Params::default_basic();
-        common::init::setup_user(&ctx, init_params).await;
-        let cb = common::init::NullCallback {};
+        setup_user(&ctx, init_params).await;
         user_ctx
             .initialize_async(LabelId::inbox().clone(), &cb)
             .await
