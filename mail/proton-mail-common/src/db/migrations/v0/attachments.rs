db214b7ae7cd0b97d9862fee6468b9f52dfe52b9
Dan Williams <dan.williams@proton.ch>
2024-04-11 17:49:13 +0100

    chore: Reformatted migration SQL queries and fixed typos

--- mail/proton-mail-common/src/db/migrations/v0/attachments.rs
+++ mail/proton-mail-common/src/db/migrations/v0/attachments.rs
@@ -4,19 +4,23 @@ pub fn create_attachment_tables(tx: &mut Transaction) -> crate::db::DBResult<()>
     // Attachments
     tx.execute(
         r#"
-CREATE TABLE attachments (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    rid TEXT UNIQUE,
-    name TEXT NON NULL,
-    size INTEGER NOT NULL,
-    mime_type TEXT NOT NULL,
-    address_id TEXT DEFAULT NULL,
-    key_patckets TEXT DEFAULT NULL,
-    signature TEXT DEFAULT NULL,
-    enc_signature TEXT DEFAULT NULL,
-    disposition TEXT NOT NULL,
-    CONSTRAINT attachments_address_id FOREIGN KEY (address_id) REFERENCES addresses (id)
-)"#,
+            CREATE TABLE attachments (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                rid TEXT UNIQUE,
+                name TEXT NON NULL,
+                size INTEGER NOT NULL,
+                mime_type TEXT NOT NULL,
+                address_id TEXT DEFAULT NULL,
+                key_patckets TEXT DEFAULT NULL,
+                signature TEXT DEFAULT NULL,
+                enc_signature TEXT DEFAULT NULL,
+                disposition TEXT NOT NULL,
+                
+                CONSTRAINT attachments_address_id
+                    FOREIGN KEY (address_id)
+                    REFERENCES addresses (id)
+            )
+        "#,
         (),
     )?;
 
