6dd7d06f9287bc606669c801d0b8a4bf0c622e87
Leander Beernaert <leander.beernaert@proton.ch>
2024-04-12 15:59:41 +0200

    fix: Return folder types when requesting movable folders

--- proton-mail-uniffi/src/mail/user_session/labels.rs
+++ proton-mail-uniffi/src/mail/user_session/labels.rs
@@ -43,7 +43,7 @@ impl MailUserSession {
     /// # Errors
     /// Returns an error if the list can not be retrieved.
     pub fn movable_folders(&self) -> Result<Vec<LocalLabel>, MailSessionError> {
-        Ok(self.ctx.get_labels_by_type(LabelType::Label)?)
+        Ok(self.ctx.movable_folders()?)
     }
 
     /// Return the list of labels of type Label that can be applied to conversations or
