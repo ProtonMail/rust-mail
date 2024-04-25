1c808f2bd2a3832d6cdeb194ac66f3c49f5561f2
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-04-25 09:54:26 +0200

    refactor(core-common): Update DB table format

--- core/proton-core-common/src/db/migrations/session/v0/mod.rs
+++ core/proton-core-common/src/db/migrations/session/v0/mod.rs
@@ -8,9 +8,17 @@ impl proton_sqlite3::Migration for V0 {
     }
     fn migrate(&self, tx: &mut SqliteTransaction) -> proton_sqlite3::rusqlite::Result<()> {
         tx.execute(
-            "CREATE TABLE core_sessions (id TEXT UNIQUE NOT NULL, \
-user_id TEXT UNIQUE NOT NULL PRIMARY KEY, email TEXT NOT NULL, name TEXT DEFAULT NULL,\
-access_token BLOB NOT NULL, refresh_token BLOB NOT NULL, key_secret BLOB DEFAULT NULL, scopes TEXT NOT NULL DEFAULT '')",
+            r"
+            CREATE TABLE core_sessions (
+                id TEXT UNIQUE NOT NULL,
+                user_id TEXT UNIQUE NOT NULL PRIMARY KEY,
+                email TEXT NOT NULL,
+                name TEXT DEFAULT NULL,
+                access_token BLOB NOT NULL,
+                refresh_token BLOB NOT NULL,
+                key_secret BLOB DEFAULT NULL,
+                scopes TEXT NOT NULL DEFAULT ''
+            )",
             (),
         )?;
 
