1900f490a737728a1eaa0d620c9bc2686aaa2d79
rlee <robert.lee@proton.ch>
2024-05-01 14:12:30 +0100

    feat(ET-140): Add basic integration test for getting sender image for a conversation

---
+++ mail/proton-mail-common/tests/sender_image.rs
@@ -0,0 +1,60 @@
+mod common;
+
+use common::init::{NullCallback, Params as TestParams};
+use common::TestContext;
+use proton_api_mail::domain::{Label, LabelId, LabelType};
+use proton_mail_common::db::LocalConversationId;
+use proton_mail_common::Mailbox;
+
+#[test]
+fn test_get_sender_image() {
+    // Set up a user and initialise the inbox
+    let ctx = TestContext::new();
+    let mut params = TestParams::default_basic();
+    params
+        .labels
+        .get_mut(&LabelType::Label)
+        .unwrap()
+        .push(Label {
+            id: LabelId::from("testlabel"),
+            parent_id: None,
+            name: "testlabel".to_string(),
+            path: None,
+            color: "".to_string(),
+            label_type: LabelType::Label,
+            notify: false,
+            display: false,
+            sticky: false,
+            expanded: false,
+            order: 0,
+        });
+    ctx.async_runtime().block_on(async {
+        let conversations = params.conversations.clone();
+        ctx.setup_user(params.clone()).await;
+        ctx.mock_get_conversations(conversations, 2).await;
+        ctx.mock_get_image_for_conversation(vec![0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07])
+            .await;
+        ctx.catch_all().await;
+        ctx.user_context()
+            .initialize_async(LabelId::inbox().clone(), &NullCallback {})
+            .await
+            .expect("failed to initialize");
+    });
+
+    // Create a mailbox
+    let mailbox = Mailbox::with_remote_id(ctx.user_context(), LabelId::inbox()).unwrap();
+
+    ctx.async_runtime().block_on(async {
+        mailbox.sync(2).await;
+    });
+    let local_conversation = mailbox.conversations(2).unwrap();
+
+    ctx.async_runtime().block_on(async {
+        let image = mailbox
+            //.rob_testing()
+            .get_image_for_conversation(local_conversation.first().unwrap().id, None, None)
+            .await
+            .unwrap();
+        assert_eq!(image, vec![0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07])
+    });
+}
