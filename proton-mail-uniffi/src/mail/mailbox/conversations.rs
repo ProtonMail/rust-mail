5852e15419eac0daf9aaeccd08af9b9b524c021d
Leander Beernaert <leander.beernaert@proton.ch>
2024-05-03 15:11:15 +0200

    fix: Fixes to get_image_for_conversation
    * Rename to `image_for_conversation`
    * Expose format parameter
    * Change return type to Option

--- proton-mail-uniffi/src/mail/mailbox/conversations.rs
+++ proton-mail-uniffi/src/mail/mailbox/conversations.rs
@@ -133,18 +133,23 @@ impl Mailbox {
 
     /// Get the sender image for a conversation.
     ///
-    /// size is used to give the x*x size of the returned image (will default to 32 if none provided)
-    /// mode can be used to select if the "light" or "dark" mode of the image is desired (default is light)
+    /// # Parameters
+    /// * `size`: Is used to give the x*x size of the returned image (will default to 32 if none provided).
+    /// * `mode`: Can be used to select if the "light" or "dark" mode of the image is desired (default is light).
+    /// * `format`: Desired image format, if none is specified the default format of the image will be used.
+    ///
+    /// Returns `None` if no image needs to be displayed.
     ///
     /// # Errors
     /// Returns errors if the API call fails, the mode value is invalid, the conversation doesn't exist, or
     /// if there's an issue with the sender that causes problems when creating the API request on our side.
-    pub async fn get_image_for_conversation(
+    pub async fn image_for_conversation(
         &self,
         conversation_id: u64,
         size: Option<u32>,
         mode: Option<String>,
-    ) -> Result<Vec<u8>, MailboxError> {
+        format: Option<String>,
+    ) -> Result<Option<Vec<u8>>, MailboxError> {
         let mode = match mode {
             Some(m) => match m.as_str() {
                 "light" | "Light" => Some(LightOrDarkMode::Light),
@@ -162,15 +167,16 @@ impl Mailbox {
             .async_runtime()
             .spawn(async move {
                 match mbox
-                    .get_image_for_conversation(
+                    .image_for_conversation(
                         LocalConversationId::from(conversation_id),
                         size,
                         mode,
+                        format,
                     )
                     .await
                     .map_err(MailboxError::from)
                 {
-                    Ok(resp) => Ok(resp.to_vec()), //TODO (ET-208) replace when we have saving to files or uniffi supports Bytes
+                    Ok(resp) => Ok(resp.map(|v| v.to_vec())), //TODO (ET-208) replace when we have saving to files or uniffi supports Bytes
                     Err(e) => Err(e),
                 }
             })
