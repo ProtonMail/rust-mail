4adcf7226156bfc91371ae23ea35b42d5ec59ed2
Leander Beernaert <leander.beernaert@proton.ch>
2024-05-08 14:12:06 +0200

    refactor: Decouple Sender Image from Message Address
    The mobile side prefers to pass in the parameters as required rather
    than keeping `MessageAddress` around.

--- proton-mail-uniffi/src/mail/user_session/images.rs
+++ proton-mail-uniffi/src/mail/user_session/images.rs
@@ -1,44 +1,15 @@
 use crate::mail::MailUserSession;
 use crate::mail::{MailSessionError, MailSessionResult};
-use proton_mail_common::proton_api_mail::domain::{LightOrDarkMode, MessageAddress};
+use proton_mail_common::proton_api_mail::domain::LightOrDarkMode;
 
 #[uniffi::export]
 impl MailUserSession {
     /// Get the sender image for a list of senders.
     ///
     /// # Parameters
-    /// * `size`: Is used to give the x*x size of the returned image (will default to 32 if none provided).
-    /// * `mode`: Can be used to select if the "light" or "dark" mode of the image is desired (default is light).
-    /// * `format`: Desired image format, if none is specified the default format of the image will be used.
-    ///
-    /// Returns `None` if no image needs to be displayed.
-    ///
-    /// # Errors
-    /// Returns errors if the API call fails, the mode value is invalid, the conversation doesn't exist, or
-    /// if there's an issue with the sender that causes problems when creating the API request on our side.
-    pub async fn image_for_senders(
-        &self,
-        senders: Vec<MessageAddress>,
-        size: Option<u32>,
-        mode: Option<String>,
-        format: Option<String>,
-    ) -> MailSessionResult<Option<Vec<u8>>> {
-        let mode = light_or_dark_mode_from_string(mode)?;
-
-        let ctx = self.ctx.clone();
-        self.uniffi_async(async move {
-            //TODO (ET-208) replace when we have saving to files or uniffi supports Bytes
-            Ok(ctx
-                .image_for_senders(&senders, size, mode, format)
-                .await
-                .map(|v| v.map(|v| v.to_vec()))?)
-        })
-        .await
-    }
-
-    /// Get the sender image for a sender address.
-    ///
-    /// # Parameters
+    /// * `address`: Email address of the sender.
+    /// * `bimi_selector`: BIMI protocol selector.
+    /// * `display_sender_image`: Whether this sender would has sender image enabled.
     /// * `size`: Is used to give the x*x size of the returned image (will default to 32 if none provided).
     /// * `mode`: Can be used to select if the "light" or "dark" mode of the image is desired (default is light).
     /// * `format`: Desired image format, if none is specified the default format of the image will be used.
@@ -50,17 +21,27 @@ impl MailUserSession {
     /// if there's an issue with the sender that causes problems when creating the API request on our side.
     pub async fn image_for_sender(
         &self,
-        sender: MessageAddress,
+        address: String,
+        bimi_selector: Option<String>,
+        display_sender_image: bool,
         size: Option<u32>,
         mode: Option<String>,
         format: Option<String>,
     ) -> MailSessionResult<Option<Vec<u8>>> {
         let mode = light_or_dark_mode_from_string(mode)?;
+
         let ctx = self.ctx.clone();
         self.uniffi_async(async move {
             //TODO (ET-208) replace when we have saving to files or uniffi supports Bytes
             Ok(ctx
-                .image_for_sender(&sender, size, mode, format)
+                .image_for_sender(
+                    address,
+                    bimi_selector,
+                    display_sender_image,
+                    size,
+                    mode,
+                    format,
+                )
                 .await
                 .map(|v| v.map(|v| v.to_vec()))?)
         })
