95aa6728e7168247c2a6950f17f42e14e040d0fa
rlee <robert.lee@proton.ch>
2024-05-13 10:06:33 +0100

    (ET-173): Update avatar initial calculation logic

--- mail/proton-mail-common/src/avatar.rs
+++ mail/proton-mail-common/src/avatar.rs
@@ -57,7 +57,7 @@ pub fn avatar_text(name: &str, email: &str) -> String {
 }
 
 fn initials(name: &str) -> String {
-    let mut s = String::with_capacity(2);
+    let mut s = String::with_capacity(1);
 
     for c in name
         .split_whitespace()
@@ -65,7 +65,7 @@ fn initials(name: &str) -> String {
     {
         s.push(c);
 
-        if s.len() == 2 {
+        if !s.is_empty() {
             break;
         }
     }
@@ -76,19 +76,16 @@ fn initials(name: &str) -> String {
 fn email_text(address: &str) -> String {
     let local = match address.trim_matches(&['<', '>']).split('@').next() {
         Some(first) => first.trim(),
-        None => return "??".to_string(),
+        None => return "?".to_owned(),
     };
 
     let mut chars = local.chars();
 
     if let Some(first) = chars.next() {
-        match chars.next() {
-            Some(second) => return format!("{}{}", first.to_uppercase(), second),
-            None => return format!("{}", first.to_uppercase()),
-        }
+        return format!("{}", first.to_uppercase());
     }
 
-    "??".to_string()
+    "?".to_owned()
 }
 
 #[cfg(test)]
@@ -97,29 +94,33 @@ mod tests {
 
     #[test]
     fn test_initials() {
-        assert_eq!(initials("John Doe"), "JD");
-        assert_eq!(initials("john doe"), "jd");
+        assert_eq!(initials("John Doe"), "J");
+        assert_eq!(initials("john doe"), "j");
         assert_eq!(initials("John"), "J");
         assert_eq!(initials(""), "");
         assert_eq!(initials("J"), "J");
-        assert_eq!(initials("John 1Doe"), "J1");
-        assert_eq!(initials("123 John"), "1J");
-        assert_eq!(initials("🙂 John Doe"), "JD");
+        assert_eq!(initials("John 1Doe"), "J");
+        assert_eq!(initials("123 John"), "1");
+        assert_eq!(initials("🙂 John Doe"), "J");
     }
 
     #[test]
     fn test_email_text() {
-        assert_eq!(email_text("brains@tracyisland.com"), "Br");
-        assert_eq!(email_text("    brains@tracyisland.com"), "Br");
+        assert_eq!(email_text("brains@tracyisland.com"), "B");
+        assert_eq!(email_text("    brains@tracyisland.com"), "B");
         assert_eq!(email_text("A@test.com"), "A");
-        assert_eq!(email_text("<brains@tracyisland.com>"), "Br");
-        assert_eq!(email_text("@nolocal.com"), "??");
+        assert_eq!(email_text("<brains@tracyisland.com>"), "B");
+        assert_eq!(email_text("@nolocal.com"), "?");
     }
 
     #[test]
     fn test_avatar_text() {
-        assert_eq!(avatar_text("Riri Fifi Loulou", "rifilou@test.com"), "Rf");
-        assert_eq!(avatar_text("🙂", "emojiname@test.com`"), "Em");
-        assert_eq!(avatar_text("OnePart", "onepart@test.com"), "O")
+        assert_eq!(avatar_text("Riri Fifi Loulou", "rifilou@test.com"), "R");
+        assert_eq!(avatar_text("🙂", "emojiname@test.com`"), "E");
+        assert_eq!(avatar_text("OnePart", "onepart@test.com"), "O");
+        assert_eq!(avatar_text("John Smith", "john@smith.com"), "J");
+        assert_eq!(avatar_text("John", "john@smith.com"), "J");
+        assert_eq!(avatar_text("", "john@smith.com"), "J");
+        assert_eq!(avatar_text("🙂 John", "emojijohn@test.com`"), "J");
     }
 }
