1f27679ac8447c9b9f31c8d4eef8f20f8bf55460
Dan Williams <dan.williams@proton.ch>
2024-06-27 06:22:34 +0100

    Refactored event data structures
      - Added API response data types and endpoint-calling functions for
        event functionality.
    
      - Added local event data types. Although not related to models, as
        events are not stored in the database, having local representation
        is still sensible - albeit in a different module.

---
+++ mail/proton-mail-common/src/events.rs
@@ -0,0 +1,276 @@
+//! Event data types for the Proton Mail common library.
+//!
+//! This module contains various data types used by the Proton Mail common
+//! library in association to events. Notably, they are NOT used by the models
+//! in the [`models`](crate::models) module, i.e. they do not represent child
+//! data structures for the models' fields, and are entirely separate from the
+//! persistent data structures.
+//!
+//! The data types used by events therefore do NOT need to be convertible to and
+//! from database-compatible format using [`ToSql`](stash::exports::ToSql) and
+//! [`FromSql`](stash::exports::FromSql). If anything in this module implements
+//! those traits, it is a sign that a mistake has been made. Additionally, they
+//! do not generally need to be serializable or deserializable, as they are not
+//! used for network communication or any other interchange purpose as a general
+//! requirement, and so implementation of [`Serialize`](serde::Serialize) and
+//! [`Deserialize`](serde::Deserialize) is not necessary and may also be a sign
+//! of a mistake.
+//!
+//! Generally speaking, [`From`] conversions to convert from the Proton API
+//! types to the internal types are provided, but not vice versa unless there is
+//! a specific need. Such conversions are usually very simple and indeed in many
+//! cases can be done without altering any data in memory.
+//!
+//! This separation does cause some duplication, but the overlap is not total.
+//! The various implementations for the types differ in each place; any logic
+//! for the application is in the application types and not the API types; and
+//! the distinction allows customisation of how the application deals with its
+//! related data. Additionally, it promotes wider usability, as each application
+//! that depends upon the API types can interpret and managed them in its own
+//! way.
+//!
+
+use crate::datatypes::{ConversationCount, MessageCount, MessageMetadata};
+use crate::models::{Conversation, Label, MailSettings};
+use proton_api_mail::services::proton::response_data::{
+    ConversationEvent as ApiConversationEvent, LabelEvent as ApiLabelEvent,
+    MailEvent as ApiMailEvent, MessageEvent as ApiMessageEvent,
+};
+use proton_core_common::datatypes::{ProductUsedSpace, RemoteId};
+use proton_core_common::events::Action;
+use proton_core_common::models::{Address, User, UserSettings};
+use proton_event_loop::Event;
+
+/// TODO: Document this struct.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ConversationEvent {
+    /// TODO: Document this field.
+    pub remote_id: RemoteId,
+
+    /// TODO: Document this field.
+    pub event_id: RemoteId,
+
+    /// TODO: Document this field.
+    pub action: Action,
+
+    /// TODO: Document this field.
+    pub conversation: Option<Conversation>,
+
+    /// TODO: Document this field.
+    pub has_more: bool,
+}
+
+impl Event for ConversationEvent {
+    type Id = RemoteId;
+    type Response = ApiConversationEvent;
+
+    fn event_id(&self) -> &Self::Id {
+        &self.event_id
+    }
+
+    fn has_more(&self) -> bool {
+        self.has_more
+    }
+}
+
+impl From<ApiConversationEvent> for ConversationEvent {
+    fn from(value: ApiConversationEvent) -> Self {
+        Self {
+            remote_id: value.id.into(),
+            event_id: value.event_id.into(),
+            action: value.action.into(),
+            conversation: value.conversation.map(Conversation::from),
+            has_more: value.has_more,
+        }
+    }
+}
+
+/// TODO: Document this struct.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct LabelEvent {
+    /// TODO: Document this field.
+    pub remote_id: RemoteId,
+
+    /// TODO: Document this field.
+    pub event_id: RemoteId,
+
+    /// TODO: Document this field.
+    pub action: Action,
+
+    /// TODO: Document this field.
+    pub has_more: bool,
+
+    /// TODO: Document this field.
+    pub label: Option<Label>,
+}
+
+impl Event for LabelEvent {
+    type Id = RemoteId;
+    type Response = ApiLabelEvent;
+
+    fn event_id(&self) -> &Self::Id {
+        &self.event_id
+    }
+
+    fn has_more(&self) -> bool {
+        self.has_more
+    }
+}
+
+impl From<ApiLabelEvent> for LabelEvent {
+    fn from(value: ApiLabelEvent) -> Self {
+        Self {
+            remote_id: value.id.into(),
+            event_id: value.event_id.into(),
+            action: value.action.into(),
+            has_more: value.has_more,
+            label: value.label.map(Label::from),
+        }
+    }
+}
+
+/// TODO: Document this struct.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct MailEvent {
+    /// TODO: Document this field.
+    pub remote_id: RemoteId,
+
+    /// TODO: Document this field.
+    pub event_id: RemoteId,
+
+    /// TODO: Document this field.
+    pub action: Action,
+
+    /// TODO: Document this field.
+    pub addresses: Option<Vec<Address>>,
+
+    /// TODO: Document this field.
+    pub conversation_counts: Option<Vec<ConversationCount>>,
+
+    /// TODO: Document this field.
+    pub conversations: Option<Vec<ConversationEvent>>,
+
+    /// TODO: Document this field.
+    pub has_more: bool,
+
+    /// TODO: Document this field.
+    pub labels: Option<Vec<LabelEvent>>,
+
+    /// TODO: Document this field.
+    pub mail_settings: Option<MailSettings>,
+
+    /// TODO: Document this field.
+    pub message_counts: Option<Vec<MessageCount>>,
+
+    /// TODO: Document this field.
+    pub messages: Option<Vec<MessageEvent>>,
+
+    /// TODO: Document this field.
+    pub product_used_space: Option<ProductUsedSpace>,
+
+    /// TODO: Document this field.
+    pub used_space: Option<i64>,
+
+    /// TODO: Document this field.
+    pub user: Option<User>,
+
+    /// TODO: Document this field.
+    pub user_settings: Option<UserSettings>,
+}
+
+impl Event for MailEvent {
+    type Id = RemoteId;
+    type Response = ApiMailEvent;
+
+    fn event_id(&self) -> &Self::Id {
+        &self.event_id
+    }
+
+    fn has_more(&self) -> bool {
+        self.has_more
+    }
+}
+
+impl From<ApiMailEvent> for MailEvent {
+    fn from(value: ApiMailEvent) -> Self {
+        Self {
+            remote_id: value.id.into(),
+            event_id: value.event_id.into(),
+            action: value.action.into(),
+            addresses: value
+                .addresses
+                .map(|addresses| addresses.into_iter().map(Address::from).collect()),
+            conversation_counts: value.conversation_counts.map(|conversation_counts| {
+                conversation_counts
+                    .into_iter()
+                    .map(ConversationCount::from)
+                    .collect()
+            }),
+            conversations: value.conversations.map(|conversations| {
+                conversations
+                    .into_iter()
+                    .map(ConversationEvent::from)
+                    .collect()
+            }),
+            has_more: value.has_more,
+            labels: value
+                .labels
+                .map(|labels| labels.into_iter().map(LabelEvent::from).collect()),
+            mail_settings: value.mail_settings.map(MailSettings::from),
+            message_counts: value
+                .message_counts
+                .map(|message_counts| message_counts.into_iter().map(MessageCount::from).collect()),
+            messages: value
+                .messages
+                .map(|messages| messages.into_iter().map(MessageEvent::from).collect()),
+            product_used_space: value.product_used_space.map(ProductUsedSpace::from),
+            used_space: value.used_space,
+            user: value.user.map(User::from),
+            user_settings: value.user_settings.map(UserSettings::from),
+        }
+    }
+}
+
+/// TODO: Document this struct.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct MessageEvent {
+    /// TODO: Document this field.
+    pub remote_id: RemoteId,
+
+    /// TODO: Document this field.
+    pub event_id: RemoteId,
+
+    /// TODO: Document this field.
+    pub action: Action,
+
+    /// TODO: Document this field.
+    pub has_more: bool,
+
+    /// TODO: Document this field.
+    pub message: Option<MessageMetadata>,
+}
+
+impl Event for MessageEvent {
+    type Id = RemoteId;
+    type Response = ApiMessageEvent;
+
+    fn event_id(&self) -> &Self::Id {
+        &self.event_id
+    }
+
+    fn has_more(&self) -> bool {
+        self.has_more
+    }
+}
+
+impl From<ApiMessageEvent> for MessageEvent {
+    fn from(value: ApiMessageEvent) -> Self {
+        Self {
+            remote_id: value.id.into(),
+            event_id: value.event_id.into(),
+            action: value.action.into(),
+            has_more: value.has_more,
+            message: value.message.map(MessageMetadata::from),
+        }
+    }
+}
