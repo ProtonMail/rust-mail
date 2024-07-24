d0ed2c4a585ad2887680965f4f5652ecb3c1f2c4
Orion Gonzalez <devardi@proton.me>
2024-07-24 08:54:11 +0200

    feat: html benchmarking

---
+++ mail/proton-mail-html-transformer/benches/parsing_html.rs
@@ -0,0 +1,29 @@
+use std::{borrow::Borrow, fs::read_to_string};
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use proton_mail_html_transformer::Transformer;
+
+pub fn parse(c: &mut Criterion) {
+    let html = include_str!("./amos_landing.html");
+    parse_inner(c, html);
+    let html = include_str!("./amos_http.html");
+    parse_inner(c, html);
+}
+
+pub fn parse_inner(c: &mut Criterion, html: &str) {
+    c.bench_function("parse html", |b| {
+        b.iter(|| Transformer::new(black_box(&html)).to_string())
+    });
+    c.bench_function("disable remote content", |b| {
+        b.iter(|| {
+            Transformer::new(black_box(&html))
+                .disable_remote_content()
+                .unwrap()
+                .to_string()
+        })
+    });
+}
+
+criterion_group!(benches, parse);
+criterion_main!(benches);
