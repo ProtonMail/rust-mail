823a3e542da8192a258662c7f16a5cebf9a0e3ef
Orion Gonzalez <devardi@proton.me>
2024-07-24 15:30:04 +0200

    feat(ET-382): HTML sanitization

--- proton-mail-html-transformer/benches/parsing_html.rs
+++ proton-mail-html-transformer/benches/parsing_html.rs
@@ -1,5 +1,4 @@
-use std::{borrow::Borrow, fs::read_to_string};
-
+#![allow(clippy::pedantic)]
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use proton_mail_html_transformer::Transformer;
@@ -13,14 +12,22 @@ pub fn parse(c: &mut Criterion) {
 
 pub fn parse_inner(c: &mut Criterion, html: &str) {
     c.bench_function("parse html", |b| {
-        b.iter(|| Transformer::new(black_box(&html)).to_string())
+        b.iter(|| Transformer::new(black_box(html)).to_string())
     });
+
     c.bench_function("disable remote content", |b| {
         b.iter(|| {
-            Transformer::new(black_box(&html))
-                .disable_remote_content()
-                .unwrap()
-                .to_string()
+            let mut t = Transformer::new(black_box(html));
+            t.disable_remote_content().unwrap();
+            t.to_string();
+        })
+    });
+
+    c.bench_function("strip", |b| {
+        b.iter(|| {
+            let t = Transformer::new(black_box(html));
+            t.strip_whitelist();
+            t.to_string();
         })
     });
 }
