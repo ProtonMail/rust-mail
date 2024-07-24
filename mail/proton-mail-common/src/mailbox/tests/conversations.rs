b9080e6ecc0164b76efbe6d7e50f3abaa7a04e11
Pawel Rynowiecki <pawel.rynowiecki@anixe.pl>
2024-07-24 13:44:05 +0200

    Fix: Always display message in the conversation

--- mail/proton-mail-common/src/mailbox/tests/conversations.rs
+++ mail/proton-mail-common/src/mailbox/tests/conversations.rs
@@ -2,6 +2,123 @@ use crate::datatypes::{LabelColor, LabelType, MessageAddress, MessageFlags, Syst
 use crate::models::{Conversation, Label, Message};
 use proton_core_common::datatypes::{LabelId, RemoteId};
 
+mod drafts {
+    use super::*;
+    use lazy_static::lazy_static;
+    use test_case::test_case;
+
+    lazy_static! {
+        static ref STARRED: Label = new_label(LabelType::System, Some(LabelId::drafts().clone()));
+        static ref LABEL: Label = new_label(LabelType::Label, None);
+        static ref FOLDER: Label = new_label(LabelType::Folder, None);
+        static ref INBOX: Label = new_label(LabelType::System, Some(LabelId::inbox().clone()));
+    }
+
+    #[test_case(
+        &[], None; "TEST1 - empty messages"
+    )]
+    #[test_case(
+        &[(MessageFlags::RECEIVED, false),], Some(0); "TEST2 - read - recieved message"
+    )]
+    #[test_case(
+        &[(MessageFlags::empty(), false),], Some(0); "TEST3 - read - draft message"
+    )]
+    #[test_case(
+        &[(MessageFlags::OPENED, false),], Some(0); "TEST4 - read - draft & opened message"
+    )]
+    #[test_case(
+        &[(MessageFlags::OPENED, true),], Some(0); "TEST5 - unread - draft & opened message"
+    )]
+    #[test_case(
+        &[(MessageFlags::RECEIVED | MessageFlags::OPENED, true),], Some(0); "TEST6 - unread - recieved & opened message"
+    )]
+    #[test_case(
+        &[(MessageFlags::RECEIVED, true),], Some(0); "TEST7 - unread - recieved message"
+    )]
+    #[test_case(
+        &[(MessageFlags::RECEIVED | MessageFlags::INTERNAL, true),], Some(0); "TEST8 - unread - recieved & internal message"
+    )]
+    #[test_case(
+        &[(MessageFlags::SENT | MessageFlags::INTERNAL, true),], Some(0); "TEST9 - unread - opened & internal message"
+    )]
+    #[test_case(
+        &[
+            (MessageFlags::RECEIVED, true),
+            (MessageFlags::RECEIVED, false),
+            (MessageFlags::RECEIVED | MessageFlags::INTERNAL | MessageFlags::OPENED, true),
+            (MessageFlags::RECEIVED | MessageFlags::INTERNAL, true),
+
+        ], Some(2); "TEST10 - all unread - recieved | internal | opened messages"
+    )]
+    #[test_case(
+        &[
+            (MessageFlags::RECEIVED, true),
+            (MessageFlags::RECEIVED, true),
+            (MessageFlags::empty(), true),
+
+        ], Some(0); "TEST11 - all unread - recieved | draft messages"
+    )]
+    #[test_case(
+        &[
+            (MessageFlags::RECEIVED, true),
+            (MessageFlags::RECEIVED, true),
+            (MessageFlags::empty(), false),
+
+        ], Some(0); "TEST12 - some unread - recieved | draft messages"
+    )]
+    #[test_case(
+        &[
+            (MessageFlags::SENT, true),
+            (MessageFlags::SENT, true),
+            (MessageFlags::empty(), false),
+
+        ], Some(0); "TEST13 - some unread - sent | draft messages"
+    )]
+    #[test_case(
+        &[
+            (MessageFlags::SENT | MessageFlags::RECEIVED, true),
+            (MessageFlags::SENT | MessageFlags::RECEIVED, true),
+            (MessageFlags::empty(), false),
+
+        ], Some(0); "TEST14 - some unread - sent & received | draft messages"
+    )]
+    #[test_case(
+        &[
+            (MessageFlags::RECEIVED, true),
+            (MessageFlags::RECEIVED, true),
+            (MessageFlags::empty(), true),
+            (MessageFlags::RECEIVED, true),
+            (MessageFlags::empty(), true),
+
+        ], Some(3); "TEST15 - all unread - received | draft messages"
+    )]
+    fn draft_messages(messages: &[(MessageFlags, bool)], expected_id: Option<u64>) {
+        let messages = messages
+            .iter()
+            .enumerate()
+            .map(|(id, (flags, unread))| message_metadata_with_flags(id as u64, *flags, *unread))
+            .collect::<Vec<_>>();
+
+        assert_eq!(
+            dbg!(Conversation::first_unread_message(&STARRED, &messages)),
+            dbg!(expected_id)
+        );
+        assert_eq!(
+            Conversation::first_unread_message(&LABEL, &messages),
+            expected_id
+        );
+        assert_eq!(
+            Conversation::first_unread_message(&FOLDER, &messages),
+            expected_id
+        );
+        assert_eq!(
+            Conversation::first_unread_message(&INBOX, &messages),
+            expected_id
+        );
+    }
+}
+
+// TODO: Extend test_cases to cover all scenarios bellow
 #[test]
 fn first_unread_conversation_message_in_starred_or_custom_label_or_folder() {
     // Messages are tagged as
