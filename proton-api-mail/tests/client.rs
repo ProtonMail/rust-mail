348c1b803d694523a8c86fa4e899d9d847dd24dd
James Houlahan <james.houlahan@proton.ch>
2024-08-27 18:33:31 +0200

    fix: Use correct page size limit logic
    The `ProtonMail::get_messages` and `ProtonMail::get_conversations`
    methods had logic to limit the requested page size to a particular
    global value (MAX_PAGE_ELEMENT_COUNT), but the comparison was backwards;
    `Ord::min` should have been used instead of `Ord::max`.

--- proton-api-mail/tests/client.rs
+++ proton-api-mail/tests/client.rs
@@ -7,6 +7,8 @@ use wiremock::{
     Mock, MockServer, ResponseTemplate,
 };
 
+type Result<T, E = Box<dyn std::error::Error + Send + Sync>> = std::result::Result<T, E>;
+
 #[cfg(test)]
 mod basic {
     use super::*;
@@ -28,5 +30,87 @@ mod basic {
         // Assert the response is as expected
         assert_eq!(status, StatusCode::OK);
         assert_eq!(body.as_ref(), b"");
+    }
+}
+
+#[cfg(test)]
+mod messages {
+    use std::{error::Error, future::Future};
+
+    use super::*;
+    use proton_api_core::{
+        services::proton::Config,
+        session::{CoreSession, Session},
+    };
+    use proton_api_mail::{
+        services::proton::{requests::GetMessagesOptions, ProtonMail},
+        MAX_PAGE_ELEMENT_COUNT_U64,
+    };
+    use serde_json::{json, Value};
+    use wiremock::matchers::path_regex;
+
+    #[tokio::test]
+    async fn get_messages_page_size_limit() -> Result<()> {
+        let server = MockServer::start().await;
+        let session = new_session(&server).await?;
+
+        // Set up mock server with expectations and mock responses
+        Mock::given(method("POST"))
+            .and(path_regex("mail/v4/messages"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(json!({
+                "Messages": [],
+                "Total": 0,
+                "Stale": 0,
+            })))
+            .expect(3)
+            .mount(&server)
+            .await;
+
+        let sizes = [
+            MAX_PAGE_ELEMENT_COUNT_U64 - 1,
+            MAX_PAGE_ELEMENT_COUNT_U64,
+            MAX_PAGE_ELEMENT_COUNT_U64 + 1,
+        ];
+
+        for size in sizes {
+            let body = get_last_body(&server, || async {
+                let mut options = GetMessagesOptions::default();
+
+                options.page_size = size;
+
+                session.api().get_messages(options).await
+            })
+            .await?;
+
+            assert!(body["PageSize"].as_u64().unwrap() <= MAX_PAGE_ELEMENT_COUNT_U64);
+        }
+
+        Ok(())
+    }
+
+    /// Create a new session which sends requests to the given mock server.
+    async fn new_session(server: &MockServer) -> Result<Session> {
+        let config = Config {
+            base_url: server.uri().parse()?,
+
+            ..Default::default()
+        };
+
+        Ok(Session::new(config, None).await?)
+    }
+
+    /// Perform some operation within a callback and return the corresponding request that was received.
+    async fn get_last_body<F, T, E>(s: &MockServer, f: impl FnOnce() -> F) -> Result<Value>
+    where
+        F: Future<Output = Result<T, E>>,
+        E: Error + Send + Sync + 'static,
+    {
+        let _ = f().await?;
+
+        let r = s.received_requests().await.ok_or("no requests received")?;
+
+        let r = r.last().ok_or("request list is empty")?;
+
+        Ok(r.body_json()?)
     }
 }
