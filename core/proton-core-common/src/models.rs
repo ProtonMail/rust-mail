f3b77792ee03936ac68f1c78f76ee2efe4a5566e
Leander Beernaert <leander.beernaert@proton.ch>
2024-08-30 13:24:30 +0200

    fix(ET-1055): ContactEmail should update on remote id conflict

--- core/proton-core-common/src/models.rs
+++ core/proton-core-common/src/models.rs
@@ -997,6 +997,54 @@ impl From<ApiContactEmail> for ContactEmail {
     }
 }
 
+impl ContactEmail {
+    /// Save a contact email to the database.
+    ///
+    /// It's imperative that you use this method over [`Model::save()`] to
+    /// ensure that existing conversations are updated.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the local conversation id is not set or the query
+    /// failed.
+    ///
+    pub async fn save(&mut self) -> Result<(), StashError> {
+        let Some(stash) = self.stash.clone() else {
+            return Err(StashError::NoStashAvailable);
+        };
+
+        self.save_using(&stash).await
+    }
+
+    /// Save a contact email to the database.
+    ///
+    /// It's imperative that you use this method over [`Model::save_using()`] to
+    /// ensure that existing conversations are updated.
+    ///
+    /// # Parameters
+    ///
+    /// * `interface` - The database interface, i.e. [`Stash`] or [`Tether`], to
+    ///                 use for finding the records.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the local conversation id is not set or the query
+    /// failed.
+    ///
+    pub async fn save_using<A>(&mut self, interface: &A) -> Result<(), StashError>
+    where
+        A: Into<AgnosticInterface> + Interface,
+    {
+        if let Some(remote_id) = self.remote_id.clone() {
+            if let Some(existing) = Self::find_by_id(remote_id, interface).await? {
+                self.row_id = existing.row_id;
+            }
+        }
+
+        <Self as Model>::save_using(self, interface).await
+    }
+}
+
 /// TODO: Document this struct.
 #[derive(Clone, Debug, Eq, Model, PartialEq)]
 #[TableName("users")]
