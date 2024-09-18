5b78420f642ef57cb022f9dc16c60bcf3ea95988
Orion Gonzalez <ardi@ardis.dev>
2024-09-18 06:49:02 +0200

    feat(ET-135, ET-1073): add functions to model

--- mail/proton-mail-common/src/models/message.rs
+++ mail/proton-mail-common/src/models/message.rs
@@ -1,6 +1,9 @@
 use super::Message;
+use crate::actions::messages::delete::Delete;
 use crate::actions::messages::label::Label;
+use crate::actions::messages::read::Read;
 use crate::actions::messages::unlabel::Unlabel;
+use crate::actions::messages::unread::Unread;
 use proton_action_queue::queue::{ActionError, ActionStatus, Queue};
 use proton_api_core::session::Session;
 use proton_core_common::datatypes::LocalId;
@@ -50,6 +53,75 @@ impl Message {
         message_ids: Vec<LocalId>,
     ) -> Result<ActionStatus<()>, ActionError<Unlabel>> {
         let action = Unlabel::new(label_id, message_ids.into_iter().map(Into::into));
+        queue.apply_action(session, action).await
+    }
+
+    /// Mark multiple messages as read.
+    ///
+    /// # Parameters
+    ///
+    /// * `session`     - The session.
+    /// * `queue`       - The action queue.
+    /// * `label_id`    - The ID of the label to apply to the messages.
+    /// * `message_ids` - The IDs of the target messages.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the API request failed.
+    ///
+    pub async fn action_mark_read(
+        session: &Session,
+        queue: &Queue,
+        label_id: LocalId,
+        message_ids: Vec<LocalId>,
+    ) -> Result<ActionStatus<()>, ActionError<Read>> {
+        let action = Read::new(label_id, message_ids);
+        queue.apply_action(session, action).await
+    }
+
+    /// Mark multiple messages as unread.
+    ///
+    /// # Parameters
+    ///
+    /// * `session`     - The session.
+    /// * `queue`       - The action queue.
+    /// * `label_id`    - The ID of the label to apply to the messages.
+    /// * `message_ids` - The IDs of the target messages.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the API request failed.
+    ///
+    pub async fn action_mark_unread(
+        session: &Session,
+        queue: &Queue,
+        label_id: LocalId,
+        message_ids: Vec<LocalId>,
+    ) -> Result<ActionStatus<()>, ActionError<Unread>> {
+        let action = Unread::new(label_id, message_ids);
+        queue.apply_action(session, action).await
+    }
+
+    /// Mark multiple messages as read.
+    ///
+    /// # Parameters
+    ///
+    /// * `session`     - The session.
+    /// * `queue`       - The action queue.
+    /// * `label_id`    - The ID of the label to apply to the messages.
+    /// * `message_ids` - The IDs of the target messages.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the API request failed.
+    ///
+    pub async fn action_delete(
+        session: &Session,
+        queue: &Queue,
+        label_id: LocalId,
+        message_ids: Vec<LocalId>,
+    ) -> Result<ActionStatus<()>, ActionError<Delete>> {
+        let action = Delete::new(label_id, message_ids);
         queue.apply_action(session, action).await
     }
 }
