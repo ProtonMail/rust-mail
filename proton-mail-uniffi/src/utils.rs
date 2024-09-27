aec415f5230596d51f38cfe8b93e13b330cc2e54
Dan Williams <dan.williams@proton.ch>
2024-09-27 09:38:42 +0100

    chore: Amended the dampening period
      - Formalised the dampening period into a constant.
    
      - Amended the period from 5secs to 200ms.
    
      - Tweaked documentation.

--- proton-mail-uniffi/src/utils.rs
+++ proton-mail-uniffi/src/utils.rs
@@ -10,9 +10,18 @@ use tokio::time::interval;
 
 use crate::LiveQueryCallback;
 
-// This returns a function that updates the boolean flag of whether we should send an update which
-// gets checked every `duration`.
-/// Like [`damp`] but takes a custom duration.
+/// Period of delay for dampening, in milliseconds. Each set of updates will be
+/// held back for up until this amount of time before the callback is triggered
+/// to notify the client.
+const DAMPENING_PERIOD: u64 = 200;
+
+/// Obtains dampening function.
+///
+/// This returns a function that updates the boolean flag of whether we should
+/// send an update which gets checked every `duration`.
+///
+/// Like [`damp()`] but takes a custom duration.
+///
 pub fn damp_with_duration(
     callback: Box<dyn LiveQueryCallback>,
     duration: Duration,
@@ -39,9 +48,14 @@ pub fn damp_with_duration(
     move || must_update.store(true, Ordering::Relaxed)
 }
 
-/// Reduces how often the given notification callback gets called to max once per 5 seconds.
-/// Returns the function you must use to actually notify the client, which you can call as often as
-/// you want.
+/// Dampens the amount of noise, i.e. number of notifications, passed through.
+///
+/// Reduces how often the given notification callback gets called to a maximum
+/// of once every dampening period.
+///
+/// It returns the function to use to actually notify the client, which you can
+/// call as often as you want.
+///
 pub fn damp(callback: Box<dyn LiveQueryCallback>) -> impl Fn() + Clone {
-    damp_with_duration(callback, Duration::from_secs(5))
+    damp_with_duration(callback, Duration::from_millis(DAMPENING_PERIOD))
 }
