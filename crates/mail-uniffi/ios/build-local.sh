46981b1f6d29cdd1379b675b63c61b2064ff05ec
James Houlahan <james@houlahan.ch>
2024-10-08 13:37:58 +0200

    ci: Correct script paths

--- crates/mail-uniffi/ios/build-local.sh
+++ crates/mail-uniffi/ios/build-local.sh
@@ -4,15 +4,15 @@
 #
 # Run from the root of the repository
 #
-# IOS_REPO_ROOT=$PATH ./proton-mail-uniffi/ios/build-local.sh
+# IOS_REPO_ROOT=$PATH ./crates/mail-uniffi/ios/build-local.sh
 
 set -eo pipefail
 
 TMP_DIR="/tmp/$(uuidgen)"
 
-rust-build/build_ios_framework_uniffi.sh proton-mail-uniffi proton-mail-uniffi/uniffi.toml $TMP_DIR
+rust-build/build_ios_framework_uniffi.sh proton-mail-uniffi ./crates/mail-uniffi/uniffi.toml $TMP_DIR
 
-CRATE_VERSION=$(cargo pkgid --manifest-path=proton-mail-uniffi/Cargo.toml | cut -d "#" -f2)
+CRATE_VERSION=$(cargo pkgid --manifest-path=./crates/mail-uniffi/Cargo.toml | cut -d "#" -f2)
 
 cp -r $TMP_DIR/$CRATE_VERSION/* $IOS_REPO_ROOT/localPackages/proton_app_uniffi/
 
