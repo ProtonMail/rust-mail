209cef6bcaad56434b6af1d58834ee276e8fa9a0
Paweł Rynowiecki <p.rynowiecki@proton.me>
2024-10-17 14:41:14 +0200

    Adjust ios & android build scripts to the new folder structure

--- mail/mail-uniffi/android/build-local.sh
+++ mail/mail-uniffi/android/build-local.sh
@@ -3,18 +3,18 @@
 #
 # Run from the root of the repository
 #
-# ./crates/mail-uniffi/android/build-local.sh
+# ./mail/mail-uniffi/android/build-local.sh
 set -eo pipefail
 
 
 # Build code
-rust-build/build_android.sh proton-mail-uniffi ./crates/mail-uniffi/uniffi.toml ./crates/mail-uniffi/android/lib/src/main/
+rust-build/build_android.sh proton-mail-uniffi ./mail/mail-uniffi/uniffi.toml ./mail/mail-uniffi/android/lib/src/main/
 # Build archive
-./crates/mail-uniffi/android/build-android-archive.sh
+./mail/mail-uniffi/android/build-android-archive.sh
 rm -rf /tmp/rust-builds
 mkdir /tmp/rust-builds/
 # Copy artifacts
-cp ./crates/mail-uniffi/android/lib/build/outputs/aar/lib-release.aar /tmp/rust-builds/
+cp ./mail/mail-uniffi/android/lib/build/outputs/aar/lib-release.aar /tmp/rust-builds/
 # Pubish
-CRATE_VERSION=$(cargo pkgid --manifest-path=./crates/mail-uniffi/Cargo.toml | cut -d "#" -f2)
+CRATE_VERSION=$(cargo pkgid --manifest-path=./mail/mail-uniffi/Cargo.toml | cut -d "#" -f2)
 mvn install:install-file -Dfile=/tmp/rust-builds/lib-release.aar -DgroupId=me.proton.mail.common -DartifactId=lib -Dversion=$CRATE_VERSION -Dpackaging=aar
--- mail/mail-uniffi/android/publish.sh
+++ mail/mail-uniffi/android/publish.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-cd ./crates/mail-uniffi/android/
+cd ./mail/mail-uniffi/android/
 
 # Set http proxy options to java tools
 export JAVA_TOOL_OPTIONS="-Dhttp.proxyHost=$( echo ${http_proxy##http://} | cut -d':' -f1 ) -Dhttp.proxyPort=$( echo ${http_proxy##http://} | cut -d':' -f2 ) -Dhttps.proxyHost=$( echo ${https_proxy##http://} | cut -d':' -f1 ) -Dhttps.proxyPort=$( echo ${https_proxy##http://} | cut -d':' -f2 ) -Dhttp.nonProxyHosts=\"$( echo $no_proxy | tr ',' '|' )\" -Dhttp.socketTimeout=600000 -Dhttp.connectionTimeout=600000 -Dhttps.socketTimeout=600000 -Dhttps.connectionTimeout=600000"
