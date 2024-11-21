44c3419c5d4409d88d0b7e37c638a50337cd06b8
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-11-21 11:00:09 +0100

    feat(ET-318): Add API send request model (json)

--- mail/mail-api/src/services/proton/responses.rs
+++ mail/mail-api/src/services/proton/responses.rs
@@ -293,3 +293,17 @@ pub struct PostCreateDraftResponse {
 pub struct PutUpdateDraftResponse {
     pub message: Message,
 }
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(any(test, debug_assertions), derive(Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct PostSendMessageResponse {
+    /// The expected epoch of delivery
+    pub delivery_time: u32,
+
+    /// The sent message
+    pub sent: Message,
+
+    /// The updated conversation.
+    pub conversation: Conversation,
+}
