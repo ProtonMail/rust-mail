25c66485754b3a4ab81603190a7aa5bfe73ceb8c
Orion Gonzalez <ardi@ardis.dev>
2024-12-04 10:49:46 +0100

    feat(ET-1672): rename logfile or append

--- mail/mail-uniffi/src/mail/logging.rs
+++ mail/mail-uniffi/src/mail/logging.rs
@@ -1,11 +1,39 @@
+use chrono::Utc;
+use std::fs::{self, OpenOptions};
 use std::path::Path;
+use tracing::error;
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Layer};
 
+/// If it finds and old log file this will add its date to it.
+fn rename_old_file(log_path: &Path) -> std::io::Result<()> {
+    if fs::exists(log_path)? {
+        let meta = fs::metadata(log_path)?;
+        let mut new = log_path.to_owned();
+        let created_date = meta.created()?;
+        let datetime: chrono::DateTime<Utc> = created_date.into();
+        let fname = log_path.file_name().unwrap();
+        let new_name = format!("{}_{fname:?}", datetime.format("%d-%m-%Y-%T"));
+        new.set_file_name(new_name);
+        fs::rename(log_path, new)?;
+    }
+    Ok(())
+}
+
 pub(super) fn init_log(log_path: &Path, debug: bool) -> std::io::Result<()> {
-    let log_file = std::fs::File::create(log_path)?;
+    let mut options = OpenOptions::new();
+    options.read(true).write(true).create(true);
+
+    // fallback to append
+    if let Err(e) = rename_old_file(log_path) {
+        error!("Error renaming old log file: {e}");
+        options.append(true);
+    }
+
+    let log_file = options.open(log_path)?;
+
     let file_subscriber = tracing_subscriber::fmt::layer()
         .with_file(false)
         .with_line_number(false)
