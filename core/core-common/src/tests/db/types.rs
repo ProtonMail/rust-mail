c9120e02fa6a9b6cf2660e3ed01f8f70e992b7aa
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2025-01-16 09:38:49 +0100

    refactor(ET-1864): Improve `account_details` tests' readability and remove duplication

--- core/core-common/src/tests/db/types.rs
+++ core/core-common/src/tests/db/types.rs
@@ -4,82 +4,84 @@ use crate::datatypes::{AccountDetails, AvatarInformation};
 use crate::db::account::CoreAccount;
 use proton_api_core::services::proton::common::UserId;
 
-#[test]
-fn test_account_details() {
-    let test_cases = vec![
-        // All fields present
-        (
-            test_account(TestAccountParams {
-                name_or_addr: "frank.moon@pm.me".to_string(),
-                display_name: Some("Frankie".to_string()),
-                username: Some("Frank Moon".to_string()),
-                primary_addr: Some("frank@proton.me".to_string()),
-            }),
-            AccountDetails {
-                name: "Frankie".to_string(),
-                email: "frank@proton.me".to_string(),
-                avatar_information: AvatarInformation::from("Frankie").into(),
-            },
-        ),
-        // No display_name, fallback to username
-        (
-            test_account(TestAccountParams {
-                name_or_addr: "Max Johnson".to_string(),
-                display_name: None,
-                username: Some("Max".to_string()),
-                primary_addr: Some("max@pm.me".to_string()),
-            }),
-            AccountDetails {
-                name: "Max".to_string(),
-                email: "max@pm.me".to_string(),
-                avatar_information: AvatarInformation::from("Max").into(),
-            },
-        ),
-        // No display_name or username, fallback to name_or_addr
-        (
-            test_account(TestAccountParams {
-                name_or_addr: "John Doe".to_string(),
-                display_name: None,
-                username: None,
-                primary_addr: Some("john@gmail.com".to_string()),
-            }),
-            AccountDetails {
-                name: "John Doe".to_string(),
-                email: "john@gmail.com".to_string(),
-                avatar_information: AvatarInformation::from("John Doe").into(),
-            },
-        ),
-        // No primary_addr, fallback to name_or_addr for email
-        (
-            test_account(TestAccountParams {
-                name_or_addr: "dricus@proton.me".to_string(),
-                display_name: Some("Dricus".to_string()),
-                username: Some("Dricus Du Plessis".to_string()),
-                primary_addr: None,
-            }),
-            AccountDetails {
-                name: "Dricus".to_string(),
-                email: "dricus@proton.me".to_string(),
-                avatar_information: AvatarInformation::from("Dricus").into(),
-            },
-        ),
-    ];
+#[cfg(test)]
+mod core_account_account_details_tests {
+    use super::*;
 
-    for (account, expected) in test_cases {
-        let result = account.account_details();
-        assert_eq!(result.name, expected.name);
-        assert_eq!(result.email, expected.email);
-        assert_eq!(result.avatar_information, expected.avatar_information);
+    #[test]
+    fn test_all_fields_present() {
+        let sut = test_account(TestCoreAccountParams {
+            name_or_addr: "frank.moon@pm.me".to_string(),
+            display_name: Some("Frankie".to_string()),
+            username: Some("Frank Moon".to_string()),
+            primary_addr: Some("frank@proton.me".to_string()),
+        });
+
+        let result = sut.account_details();
+
+        assert_account_details(result, "Frankie", "frank@proton.me");
+    }
+
+    #[test]
+    fn test_no_display_name_fallback_to_username() {
+        let sut = test_account(TestCoreAccountParams {
+            name_or_addr: "Max Johnson".to_string(),
+            display_name: None,
+            username: Some("Max".to_string()),
+            primary_addr: Some("max@pm.me".to_string()),
+        });
+
+        let result = sut.account_details();
+
+        assert_account_details(result, "Max", "max@pm.me");
+    }
+
+    #[test]
+    fn test_no_display_name_or_username_fallback_to_name_or_addr() {
+        let sut = test_account(TestCoreAccountParams {
+            name_or_addr: "John Doe".to_string(),
+            display_name: None,
+            username: None,
+            primary_addr: Some("john@gmail.com".to_string()),
+        });
+
+        let result = sut.account_details();
+
+        assert_account_details(result, "John Doe", "john@gmail.com");
+    }
+
+    #[test]
+    fn test_no_primary_addr_fallback_to_name_or_addr() {
+        let sut = test_account(TestCoreAccountParams {
+            name_or_addr: "dricus@proton.me".to_string(),
+            display_name: Some("Dricus".to_string()),
+            username: Some("Dricus Du Plessis".to_string()),
+            primary_addr: None,
+        });
+
+        let result = sut.account_details();
+
+        assert_account_details(result, "Dricus", "dricus@proton.me");
+    }
+
+    fn assert_account_details(result: AccountDetails, expected_name: &str, expected_email: &str) {
+        assert_eq!(result.name, expected_name);
+        assert_eq!(result.email, expected_email);
+        assert_eq!(
+            result.avatar_information,
+            AvatarInformation::from(expected_name).into()
+        );
     }
 }
-struct TestAccountParams {
+
+struct TestCoreAccountParams {
     name_or_addr: String,
     display_name: Option<String>,
     username: Option<String>,
     primary_addr: Option<String>,
 }
 
-fn test_account(params: TestAccountParams) -> CoreAccount {
+fn test_account(params: TestCoreAccountParams) -> CoreAccount {
     CoreAccount {
         remote_id: UserId::new("__NOT_USED__".to_string()),
         name_or_addr: params.name_or_addr,
