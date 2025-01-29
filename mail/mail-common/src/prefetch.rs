b81885b6babb5aa81a43eb30107647b43a49d054
Paweł Rynowiecki <pawel.rynowiecki@external.proton.ch>
2025-01-29 11:47:46 +0100

    feat(ET-1953): Add Component for prefetching key locations.
    This component responsibility is to work in background and fetch 10 most recent conversation/messages in each location.
    Component responsibility is to do it in the least blocking and affecting user experiance fashion. With this in mind component does yield to the executor every read/write
    to avoid saturation of resources.

---
+++ mail/mail-common/src/prefetch.rs
@@ -0,0 +1,153 @@
+use std::sync::Arc;
+
+use proton_api_core::services::proton::common::LabelId;
+use proton_core_common::{
+    datatypes::SystemLabel,
+    models::{Label, ModelIdExtension},
+};
+use stash::orm::Model;
+use tokio::task::yield_now;
+
+use crate::{
+    datatypes::ReadFilter,
+    mail_scroller::MailScroller,
+    models::{Conversation, Message},
+    MailContextError, MailUserContext,
+};
+
+pub struct Prefetch {
+    ctx: Arc<MailUserContext>,
+    prefetch_count: u64,
+    prefetch_locations: Vec<Location>,
+}
+
+struct Location {
+    label_id: LabelId,
+    location: LocationKind,
+}
+
+enum LocationKind {
+    Conversations,
+    Messages,
+}
+
+impl Prefetch {
+    pub fn key_locations(ctx: Arc<MailUserContext>) {
+        let prefetch_count = 10;
+        let locations = vec![
+            Location {
+                label_id: SystemLabel::Inbox.remote_id(),
+                location: LocationKind::Conversations,
+            },
+            Location {
+                label_id: SystemLabel::Sent.remote_id(),
+                location: LocationKind::Messages,
+            },
+            Location {
+                label_id: SystemLabel::AllSent.remote_id(),
+                location: LocationKind::Messages,
+            },
+            Location {
+                label_id: SystemLabel::Drafts.remote_id(),
+                location: LocationKind::Messages,
+            },
+            Location {
+                label_id: SystemLabel::AllDrafts.remote_id(),
+                location: LocationKind::Messages,
+            },
+        ];
+
+        let this = Self {
+            ctx,
+            prefetch_count,
+            prefetch_locations: locations,
+        };
+
+        tokio::spawn(async move {
+            let _ = this.prefetch().await;
+        });
+    }
+
+    async fn prefetch(self) -> Result<(), MailContextError> {
+        let mut tether = self.ctx.user_stash().connection();
+
+        for Location { label_id, location } in &self.prefetch_locations {
+            yield_now().await;
+            match location {
+                LocationKind::Conversations => {
+                    let Some(local_label_id) =
+                        Label::remote_id_counterpart(label_id.clone(), &tether).await?
+                    else {
+                        continue;
+                    };
+                    let Ok(mut scroller) = MailScroller::conversations(
+                        self.ctx.clone(),
+                        local_label_id,
+                        ReadFilter::All,
+                        50,
+                    )
+                    .await
+                    else {
+                        continue;
+                    };
+                    yield_now().await;
+
+                    let items = scroller.fetch_more().await?;
+
+                    yield_now().await;
+                    for item in items.into_iter().take(self.prefetch_count as usize) {
+                        let api = self.ctx.api();
+                        let _ = Conversation::sync_conversation_messages(
+                            item.local_id,
+                            &mut tether,
+                            api,
+                        )
+                        .await;
+                        yield_now().await;
+                        let messages = Message::in_conversation(item.local_id, &tether).await?;
+                        yield_now().await;
+                        let Some(label) = Label::load(local_label_id, &tether).await? else {
+                            continue;
+                        };
+                        let Ok(message_id_to_open) =
+                            Conversation::message_id_to_open(item.local_id, &label, &messages)
+                        else {
+                            continue;
+                        };
+                        yield_now().await;
+
+                        let _ = Message::message_body(self.ctx.clone(), message_id_to_open).await;
+                        yield_now().await;
+                    }
+                }
+                LocationKind::Messages => {
+                    let Some(local_label_id) =
+                        Label::remote_id_counterpart(label_id.clone(), &tether).await?
+                    else {
+                        continue;
+                    };
+                    let Ok(mut scroller) = MailScroller::messages(
+                        self.ctx.clone(),
+                        local_label_id,
+                        ReadFilter::All,
+                        50,
+                    )
+                    .await
+                    else {
+                        continue;
+                    };
+                    yield_now().await;
+                    let items = scroller.fetch_more().await?;
+                    yield_now().await;
+                    for item in items.into_iter().take(self.prefetch_count as usize) {
+                        let _ =
+                            Message::message_body(self.ctx.clone(), item.local_id.unwrap()).await;
+                        yield_now().await;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
