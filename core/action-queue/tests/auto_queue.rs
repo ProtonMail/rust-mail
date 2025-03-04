5b32f6256dd933ea2a080cc8c6f501785b3ea229
Pawel Rynowiecki <pawel.rynowiecki@external.proton.ch>
2025-03-04 14:27:01 +0100

    Revert "fix: Connection pragmas and transaction watcher tracking"
    This reverts commit 17d4f671cb3261c88ba49e755ff279f6c0f4e7bc.

--- core/action-queue/tests/auto_queue.rs
+++ core/action-queue/tests/auto_queue.rs
@@ -42,6 +42,10 @@ async fn auto_queued_on_pause() {
 
     assert!(matches!(output, BroadcastMessage::Success(_)));
     assert!(broadcast.is_empty());
+
+    auto_executor.terminate();
+    sleep(Duration::from_millis(100)).await;
+
     assert_eq!(queue.queued_actions_count().await.unwrap(), 0);
 }
 
@@ -63,6 +67,10 @@ async fn auto_queued_on_multiple_unpause() {
 
     assert!(matches!(output, BroadcastMessage::Success(_)));
     assert!(broadcast.is_empty());
+
+    auto_executor.terminate();
+    sleep(Duration::from_millis(100)).await;
+
     assert_eq!(queue.queued_actions_count().await.unwrap(), 0);
 }
 
@@ -90,6 +98,10 @@ async fn auto_queued_on_multiple_pause() {
 
     assert!(matches!(output, BroadcastMessage::Success(_)));
     assert!(broadcast.is_empty());
+
+    auto_executor.terminate();
+    sleep(Duration::from_millis(100)).await;
+
     assert_eq!(queue.queued_actions_count().await.unwrap(), 0);
 }
 
@@ -120,6 +132,10 @@ async fn auto_queued_on_pause_and_partially_manual_execution() {
 
     assert!(matches!(output, BroadcastMessage::Success(_)));
     assert!(broadcast.is_empty());
+
+    auto_executor.terminate();
+    sleep(Duration::from_millis(100)).await;
+
     assert_eq!(queue.queued_actions_count().await.unwrap(), 0);
 }
 
