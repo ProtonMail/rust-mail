88628490603c2778a28cc49880f68fd22952d72a
Orion Gonzalez <oriongonza42@pm.me>
2025-03-24 12:50:30 +0100

    refactor: derive TryFrom<u8> for SystemLabel

--- core/core-common/src/datatypes/system_label.rs
+++ core/core-common/src/datatypes/system_label.rs
@@ -2,6 +2,7 @@ use std::fmt::Display;
 
 use crate::datatypes::LocalLabelId;
 use crate::models::{Label, ModelIdExtension};
+use derive_more::TryFrom;
 use serde::{Deserialize, Serialize};
 use stash::stash::{StashError, Tether};
 
@@ -20,7 +21,10 @@ use crate::datatypes::{LabelId, LabelType};
 /// knowing that are built-in, predefined labels.
 ///
 /// In the future this enum might be extended by labels from other contexts
-#[derive(Copy, Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(
+    Copy, Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize, TryFrom,
+)]
+#[try_from(repr)]
 #[repr(u8)]
 pub enum SystemLabel {
     Inbox = 0,
@@ -33,10 +37,13 @@ pub enum SystemLabel {
     Sent = 7,
     Drafts = 8,
     Outbox = 9,
+
     Starred = 10,
-    Scheduled = 12,
     AlmostAllMail = 15,
+
+    Scheduled = 12,
     Snoozed = 16,
+
     CategorySocial = 20,
     CategoryPromotions = 21,
     CatergoryUpdates = 22,
@@ -56,29 +63,7 @@ impl SystemLabel {
     #[must_use]
     pub fn from_rid(label_id: Option<&LabelId>) -> Option<Self> {
         let remote_id = label_id?.parse::<u8>().ok()?;
-
-        match remote_id {
-            x if x == Self::Inbox as u8 => Some(Self::Inbox),
-            x if x == Self::AllDrafts as u8 => Some(Self::AllDrafts),
-            x if x == Self::AllSent as u8 => Some(Self::AllSent),
-            x if x == Self::Trash as u8 => Some(Self::Trash),
-            x if x == Self::Spam as u8 => Some(Self::Spam),
-            x if x == Self::AllMail as u8 => Some(Self::AllMail),
-            x if x == Self::Archive as u8 => Some(Self::Archive),
-            x if x == Self::Sent as u8 => Some(Self::Sent),
-            x if x == Self::Drafts as u8 => Some(Self::Drafts),
-            x if x == Self::Outbox as u8 => Some(Self::Outbox),
-            x if x == Self::Starred as u8 => Some(Self::Starred),
-            x if x == Self::Scheduled as u8 => Some(Self::Scheduled),
-            x if x == Self::AlmostAllMail as u8 => Some(Self::AlmostAllMail),
-            x if x == Self::Snoozed as u8 => Some(Self::Snoozed),
-            x if x == Self::CategorySocial as u8 => Some(Self::CategorySocial),
-            x if x == Self::CategoryPromotions as u8 => Some(Self::CategoryPromotions),
-            x if x == Self::CatergoryUpdates as u8 => Some(Self::CatergoryUpdates),
-            x if x == Self::CategoryForums as u8 => Some(Self::CategoryForums),
-            x if x == Self::CategoryDefault as u8 => Some(Self::CategoryDefault),
-            _ => None,
-        }
+        Self::try_from(remote_id).ok()
     }
 
     #[must_use]
