00a1729c36fae4cafacb6cb502c7e95e9dc3997b
Leander Beernaert <leander.beernaert@proton.ch>
2025-04-10 09:50:15 +0200

    Revert "fix: Make `Draft::save` & `Draft::send` action's `apply_remote` non pausable futures"
    This reverts commit e44082d719f9f204b92a02833f2e98e40a5e7b97.

--- mail/mail-common/src/background_execution.rs
+++ mail/mail-common/src/background_execution.rs
@@ -41,8 +41,6 @@ impl BackgroundExecutionContext {
         abort: impl Future<Output = bool>,
         max_duration: Duration,
     ) -> MailContextResult<BackgroundExecutionStatus> {
-        tracing::debug!("Background execution is gathering user contexts");
-
         let all_user_ctxs = ctx
             .get_all_logged_in_and_initialized_user_contexts()
             .await
@@ -54,8 +52,6 @@ impl BackgroundExecutionContext {
             tracing::warn!("There are no logged in users, skipping background execution");
             return Ok(BackgroundExecutionStatus::SkippedNoActiveContexts);
         }
-
-        tracing::debug!("Background execution is creating executors");
 
         // Create new executors for all the contexts.
         let queue_executors = all_user_ctxs
