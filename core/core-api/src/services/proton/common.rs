b7fb53b88315e9adc5368e4c7d30bbbd6cfb7309
James Houlahan <james.houlahan@proton.ch>
2025-04-23 18:35:59 +0200

    chore: Bump muon to improve HTTP2 error handling
    https://gitlab.protontech.ch/jhoulahan/muon/-/merge_requests/2

--- core/core-api/src/services/proton/common.rs
+++ core/core-api/src/services/proton/common.rs
@@ -87,7 +87,7 @@ impl From<muon::Error> for ApiServiceError {
             Tls | Resolve | Dial | Connect => Self::ConnectionError(e.to_string()),
 
             // Network errors.
-            Send | Closed => Self::NetworkError(format!("{e:#?}")),
+            Send | SendRetry => Self::NetworkError(format!("{e:#?}")),
 
             // Request errors.
             Req => Self::RequestError(e.to_string()),
