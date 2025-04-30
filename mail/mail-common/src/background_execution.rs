8df8c2da544b551b6738a80fbd2b6a721c81c8d0
Leander Beernaert <leander.beernaert@proton.ch>
2025-04-30 15:00:10 +0200

    feat*: Background Execution in TUI
    Simulate background execution in the TUI. Press F8 in the mailbox to
    enter the background execution view.
    
    This will simulate the transition from foreground to background and
    the background work can then be started on demand.

--- mail/mail-common/src/background_execution.rs
+++ mail/mail-common/src/background_execution.rs
@@ -2,6 +2,7 @@ use crate::{MailContext, MailContextResult};
 use std::sync::Arc;
 use std::time::Duration;
 
+#[derive(Debug)]
 pub enum BackgroundExecutionStatus {
     /// Skipped due to the lack of logged in and initialized user contexts.
     SkippedNoActiveContexts,
