ea91bd74259a85cd7cc27cfe734e0d4d03e8a073
Patryk Wychowaniec <pwychowaniec@pm.me>
2025-05-21 09:28:35 +0200

    feat*: ical/bin: Support transcoding
    This commit extends the proton-ical's binary to support both loading
    and writing *.ics, e.g. this:
    
    ```
    cargo run -p proton-ical -- foo.ics foo.2.ics
    ```
    
    ... will deserialize `foo.ics` it into `VCalendar` and then serialize it
    back into `foo.2.ics`; this comes handy for debugging.

--- core/ical/src/main.rs
+++ core/ical/src/main.rs
@@ -1,17 +1,18 @@
 #![allow(clippy::print_stdout)]
 
-use proton_ical::VCalendar;
+use proton_ical::{VCalendar, ValidatedVCalendar};
 use std::{env, fs};
 
 fn main() {
-    let path = env::args().nth(1).expect("usage:\nical <path>");
+    let src_path = env::args().nth(1).expect("usage:\nical <src> [dst]");
+    let dst_path = env::args().nth(2);
 
-    let src = fs::read(&path).unwrap_or_else(|err| {
-        panic!("couldn't read `{path}`: {err}");
+    let src = fs::read(&src_path).unwrap_or_else(|err| {
+        panic!("couldn't read `{src_path}`: {err}");
     });
 
     let out = VCalendar::from_bytes(&src).unwrap_or_else(|err| {
-        panic!("couldn't parse `{path}`: {err:?}");
+        panic!("couldn't parse `{src_path}`: {err:?}");
     });
 
     if !out.msgs.is_empty() {
@@ -31,4 +32,15 @@ fn main() {
     }
 
     println!("{:#?}", out.cal);
+
+    if let Some(dst_path) = dst_path {
+        let cal = match out.cal.validate() {
+            ValidatedVCalendar::Clean(cal) => cal.to_string(),
+            ValidatedVCalendar::Dirty(cal) => cal.to_string(),
+        };
+
+        fs::write(&dst_path, &cal).unwrap_or_else(|err| {
+            panic!("couldn't write `{dst_path}`: {err}");
+        });
+    }
 }
