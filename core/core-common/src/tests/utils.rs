9c919d6379cee70e17a566ae78380f97e6de2604
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2025-05-28 14:41:46 +0200

    fix(ET-3021): Update list of proton colors used for avatars

--- core/core-common/src/tests/utils.rs
+++ core/core-common/src/tests/utils.rs
@@ -14,8 +14,8 @@ fn test_first_grapheme_uppercase(s: &str) -> String {
 
 #[test]
 fn test_proton_color() {
-    assert_eq!(proton_color("John Doe"), "#3C8B8C");
-    assert_eq!(proton_color("Jane Doe"), "#0F735A");
-    assert_eq!(proton_color("Test"), "#A839A4");
-    assert_eq!(proton_color(""), "#0F735A");
+    assert_eq!(proton_color("John Doe"), "#3F8B8E");
+    assert_eq!(proton_color("Jane Doe"), "#2E8378");
+    assert_eq!(proton_color("Test"), "#A1439F");
+    assert_eq!(proton_color(""), "#2E8378");
 }
