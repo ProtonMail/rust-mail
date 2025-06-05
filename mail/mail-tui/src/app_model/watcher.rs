5bbde9c3cda892dabff0cee3a303e2e4c19adb86
Orion Gonzalez <oriongonza42@pm.me>
2025-06-05 15:59:35 +0200

    refactor*: Remove unused kill switch for tui watch handles

--- mail/mail-tui/src/app_model/watcher.rs
+++ mail/mail-tui/src/app_model/watcher.rs
@@ -1,6 +1,5 @@
 use crate::app::Command;
 use crate::messages::Messages;
-use flume::Sender;
 use futures::FutureExt;
 use futures::future::BoxFuture;
 use sqlite_watcher::watcher::DropRemoveTableObserverHandle;
@@ -8,7 +7,6 @@ use tracing::error;
 
 /// Handle which on drop terminates the observation of database changes.
 pub struct WatchHandle {
-    _sender: Sender<()>,
     _handle: DropRemoveTableObserverHandle,
 }
 impl WatchHandle {
@@ -18,39 +16,23 @@ impl WatchHandle {
         handle: DropRemoveTableObserverHandle,
         converter: impl Fn(T) -> BoxFuture<'static, Option<Messages>> + Send + 'static,
     ) -> (Self, Command<Messages>) {
-        let (control_sender, control_receiver) = flume::bounded(0);
-
         let command = Command::background_task(|background_sender| {
             async move {
-                loop {
-                    tokio::select! {
-                        _ = control_receiver.recv_async() => {
-                            // Terminate watcher
+                while let Ok(value) = receiver.recv_async().await {
+                    if let Some(message) = converter(value).await {
+                        if background_sender
+                            .send_async(Command::message(message))
+                            .await
+                            .is_err()
+                        {
+                            error!("Failed to send message from watcher");
                             return;
-                        }
-
-                        result = receiver.recv_async() => {
-                            let Ok(value) = result else {
-                                return;
-                            };
-
-                            if let Some(message) = converter(value).await {
-                                if background_sender.send_async(Command::message(message)).await.is_err() {
-                                    error!("Failed to send message from watcher");
-                                    return;
-                                }
-                            }
                         }
                     }
                 }
-            }.boxed()
+            }
+            .boxed()
         });
-        (
-            Self {
-                _sender: control_sender,
-                _handle: handle,
-            },
-            command,
-        )
+        (Self { _handle: handle }, command)
     }
 }
