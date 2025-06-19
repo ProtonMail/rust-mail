98d363fe6b98cec15a612eed888abbb8139954bb
Patryk Wychowaniec <pwychowaniec@pm.me>
2025-06-19 09:50:41 +0200

    refactor*: Remove mockall
    Seems to be basically unused and all the traits to which mockall was
    attached can as well be just `struct DummyWhatever;` paired with `impl
    Whatever for DummyWhatever`.

--- core/event-loop/src/provider.rs
+++ core/event-loop/src/provider.rs
@@ -7,10 +7,8 @@ use proton_core_api::service::ApiServiceError;
 use proton_core_api::services::proton::EventId;
 
 /// This trait allows abstraction over how to request the next event from the API.
-#[cfg_attr(test, mockall::automock)]
 #[async_trait]
 pub trait Provider: Send + Sync {
     async fn get_latest_event_id(&self) -> Result<EventId, ApiServiceError>;
-
     async fn get_event(&self, event_id: &EventId) -> Result<RawEvent, ApiServiceError>;
 }
