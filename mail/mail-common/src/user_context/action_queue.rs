f718e2193bb010a0adda58c112a48d4e43b3c392
Patryk Wychowaniec <pwychowaniec@pm.me>
2025-08-11 10:18:55 +0200

    chore*: review: Drop extra `#[instrument]`

--- mail/mail-common/src/user_context/action_queue.rs
+++ mail/mail-common/src/user_context/action_queue.rs
@@ -2,10 +2,8 @@ use crate::actions::MailActionError;
 use crate::{MailContextResult, MailUserContext};
 use proton_action_queue::action::Action;
 use proton_action_queue::queue::QueuedActionOutput;
-use tracing::instrument;
 
 impl MailUserContext {
-    #[instrument(skip_all)]
     pub async fn queue_action<T>(&self, action: T) -> MailContextResult<QueuedActionOutput<T>>
     where
         T: Action<Error = MailActionError>,
