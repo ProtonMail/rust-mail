ace2575afca7d2327d8125ffbe93a863e0de6786
Patryk Wychowaniec <pwychowaniec@pm.me>
2025-08-11 10:12:29 +0200

    refactor*: mail: Drop no-op comment

--- mail/mail-common/src/user_context/action_queue.rs
+++ mail/mail-common/src/user_context/action_queue.rs
@@ -2,17 +2,14 @@ use crate::actions::MailActionError;
 use crate::{MailContextResult, MailUserContext};
 use proton_action_queue::action::Action;
 use proton_action_queue::queue::QueuedActionOutput;
+use tracing::instrument;
 
 impl MailUserContext {
-    /// Queue an action for later execution.
-    ///
-    /// # Errors
-    ///
-    /// Return error if the action could not be queued.
-    pub async fn queue_action<T: Action<Error = MailActionError>>(
-        &self,
-        action: T,
-    ) -> MailContextResult<QueuedActionOutput<T>> {
+    #[instrument(skip_all)]
+    pub async fn queue_action<T>(&self, action: T) -> MailContextResult<QueuedActionOutput<T>>
+    where
+        T: Action<Error = MailActionError>,
+    {
         Ok(self.user_context.queue().queue_action(action).await?)
     }
 }
