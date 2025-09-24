f664d4b736bad30d465518e7c4a8232d479eed8c
Leander Beernaert <leander.beernaert@proton.ch>
2025-09-24 11:20:53 +0200

    refactor: Fetch and apply events 1 by 1
    We have observed that under certain circumstances event grouping can
    include up to 20 events, which can lead to large delay before the user
    sees changes as we need to make 20 separate network requests in those
    cases.
    
    Now we fetch the events 1 by 1 and apply them as they arrive so the
    feedback to the user will be more immediate, even if the changes are not
    yet complete.

--- core/event-loop/src/poll.rs
+++ core/event-loop/src/poll.rs
@@ -155,92 +155,70 @@ impl EventPollInternal {
         let mut previous_event_id = last_event_id.clone();
 
         let mut processed_event_count = 0_usize;
+        let mut has_more = true;
 
-        while processed_event_count < max_events {
-            let raw_events: Vec<RawEvent> = self
-                .collect_raw_events(provider, &previous_event_id)
+        // Keep collecting events from the same group, even if we exceed the upper limit
+        //
+        // If the events are related to a state update the `has_more` field will be set to true.
+        //
+        // E.g.:
+        // ```skip
+        // Event1 {id:2, has_more=true}  \
+        //                                +- related
+        // Event2 {id:3, has_more=false} /
+        // Event3 {id:4, has_more=false} - new event unrelated
+        // Event4 {id:4, has_more=false} - no more events
+        // ```
+        while processed_event_count < max_events || has_more {
+            let Some(raw_event) = self
+                .fetch_event(provider, &previous_event_id)
                 .await
-                .map_err(|e| {
+                .inspect_err(|e| {
                     error!("Failed to collect events: {e}");
-                    e
-                })?;
-
-            if raw_events.is_empty() {
+                })?
+            else {
                 info!("No new api events");
                 return Ok(());
+            };
+
+            info!("Received new event");
+            let new_event_id = raw_event.event_id().clone();
+            has_more = raw_event.has_more();
+            info!("Applying {:?}", previous_event_id);
+            if raw_event.is_refresh() {
+                self.publish_raw_refresh_to_subscribers(&raw_event, subscribers.values())
+                    .await?;
+            } else {
+                self.publish_raw_events_to_subscribers(&mut [raw_event], subscribers.values())
+                    .await?;
             }
 
-            let raw_event_count = raw_events.len();
-
-            info!("Received {} new events", raw_event_count);
-            // Run events 1 at a time to make sure dependencies are processed correctly.
-            for raw_event in raw_events {
-                let new_event_id = raw_event.event_id().clone();
-                info!("Applying {:?}", previous_event_id);
-                if raw_event.is_refresh() {
-                    self.publish_raw_refresh_to_subscribers(&raw_event, subscribers.values())
-                        .await?;
-                } else {
-                    self.publish_raw_events_to_subscribers(&mut [raw_event], subscribers.values())
-                        .await?;
-                }
-
-                if let Err(e) = store.store(new_event_id.clone()).await {
-                    error!("Failed to store new event id: {e}");
-                    return Err(EventLoopError::StoreWrite(e));
-                }
-                info!("New Event ID = {}", new_event_id);
-                previous_event_id = new_event_id;
+            if let Err(e) = store.store(new_event_id.clone()).await {
+                error!("Failed to store new event id: {e}");
+                return Err(EventLoopError::StoreWrite(e));
             }
-
-            processed_event_count += raw_event_count;
+            info!("New Event ID = {}", new_event_id);
+            previous_event_id = new_event_id;
+            processed_event_count += 1;
         }
 
         Ok(())
     }
 
-    /// Collect events that are part of the same "group"/update.
-    ///
-    /// If the events are related to a state update the `has_more` field will be set to true.
-    ///
-    /// E.g.:
-    /// ```skip
-    /// Event1 {id:2, has_more=true}  \
-    ///                                +- related
-    /// Event2 {id:3, has_more=false} /
-    /// Event3 {id:4, has_more=false} - new event unrelated
-    /// Event4 {id:4, has_more=false} - no more events
-    /// ```
-    async fn collect_raw_events(
+    async fn fetch_event(
         &self,
         provider: &dyn Provider,
         last_event_id: &EventId,
-    ) -> Result<Vec<RawEvent>, ApiServiceError> {
-        let mut events = Vec::with_capacity(4);
-        let mut current_event_id = last_event_id.clone();
+    ) -> Result<Option<RawEvent>, ApiServiceError> {
+        let event = provider.get_event(last_event_id).await?;
+        let new_event_id = event.event_id().clone();
 
-        loop {
-            let event = provider.get_event(&current_event_id).await?;
-            let has_more = event.has_more();
-            let new_event_id = event.event_id().clone();
-
-            // If this is the same event ID, we don't have new events
-            if new_event_id == current_event_id {
-                break;
-            }
-
-            events.push(event);
-
-            // This event "group" is now complete, lets process it before
-            // collecting more
-            if !has_more {
-                break;
-            }
-
-            current_event_id = new_event_id;
+        // If this is the same event ID, we don't have new events
+        if new_event_id == *last_event_id {
+            Ok(None)
+        } else {
+            Ok(Some(event))
         }
-
-        Ok(events)
     }
 
     async fn publish_raw_events_to_subscribers(
@@ -416,12 +394,6 @@ mod tests {
             .with(predicate::eq(event_id_1.clone()))
             .returning(move |_| Ok(event.clone()));
         let event = raw_event_2.clone();
-        provider
-            .expect_get_event()
-            .once()
-            .in_sequence(&mut sequence)
-            .with(predicate::eq(event_id_2.clone()))
-            .returning(move |_| Ok(event.clone()));
         let id = event_id_2.clone();
         subscriber
             .expect_on_raw_events()
@@ -435,8 +407,14 @@ mod tests {
             .expect_store()
             .once()
             .in_sequence(&mut sequence)
-            .with(predicate::eq(event_id_2))
+            .with(predicate::eq(event_id_2.clone()))
             .returning(|_| Ok(()));
+        provider
+            .expect_get_event()
+            .once()
+            .in_sequence(&mut sequence)
+            .with(predicate::eq(event_id_2.clone()))
+            .returning(move |_| Ok(event.clone()));
         let id = event_id_3.clone();
         subscriber
             .expect_on_raw_events()
@@ -542,12 +520,6 @@ mod tests {
             .with(predicate::eq(event_id_1.clone()))
             .returning(move |_| Ok(event.clone()));
         let event = raw_event_2.clone();
-        provider
-            .expect_get_event()
-            .once()
-            .in_sequence(&mut sequence)
-            .with(predicate::eq(event_id_2.clone()))
-            .returning(move |_| Ok(event.clone()));
         let id = event_id_2.clone();
         subscriber
             .expect_on_raw_events()
@@ -561,8 +533,14 @@ mod tests {
             .expect_store()
             .once()
             .in_sequence(&mut sequence)
-            .with(predicate::eq(event_id_2))
+            .with(predicate::eq(event_id_2.clone()))
             .returning(|_| Ok(()));
+        provider
+            .expect_get_event()
+            .once()
+            .in_sequence(&mut sequence)
+            .with(predicate::eq(event_id_2.clone()))
+            .returning(move |_| Ok(event.clone()));
         let id = event_id_3.clone();
         subscriber
             .expect_on_raw_events()
