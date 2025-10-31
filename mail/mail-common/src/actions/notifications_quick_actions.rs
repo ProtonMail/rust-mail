984245e0fffcfd0260680e933a19a28b57610ee5
Wojciech Polak <wojciech.polak@external.proton.ch>
2025-10-31 13:49:59 +0100

    chore(ET-3605): Revert API-first approach
    # Reason
    
    The API-first approach completely skips local resolution of the action.
    If you don't have an internet (and even in the case where message is
    already present in the Database), you won't ever see a change until
    network re-occurs. In the next commit message I will make different
    approach to the solution

--- mail/mail-common/src/actions/notifications_quick_actions.rs
+++ mail/mail-common/src/actions/notifications_quick_actions.rs
@@ -1,4 +1,5 @@
 use crate::datatypes::SystemLabelId;
+use crate::models::Message;
 use crate::{
     MailContextError, MailContextResult, MailUserContext,
     datatypes::mail_notifications::PushNotificationQuickAction,
@@ -8,10 +9,16 @@ use proton_action_queue::action::{
 };
 use proton_core_api::services::proton::LabelId;
 use proton_core_api::session::Session;
+use proton_core_common::datatypes::SystemLabel;
+use proton_core_common::models::LabelError;
 use proton_mail_api::services::proton::ProtonMail;
+use proton_mail_api::services::proton::common::MessageId;
 use serde::{Deserialize, Serialize};
 use stash::stash::Bond;
 use tracing::{info, instrument, warn};
+
+use super::ActionMoveData;
+use super::messages::{Move, Read};
 
 #[instrument(skip(ctx))]
 pub async fn exec(
@@ -20,40 +27,81 @@ pub async fn exec(
 ) -> MailContextResult<()> {
     info!("Executing notification action");
 
-    if let Err(err) = act(ctx.session(), &action).await {
-        warn!(
-            ?err,
-            "Couldn't act on the message, queueing action for later"
-        );
-
-        ctx.action_queue()
-            .queue_action(PushNotificationAction { action })
-            .await?;
+    match action {
+        PushNotificationQuickAction::MarkAsRead { remote_id } => {
+            read_msg(ctx, remote_id).await?;
+        }
+        PushNotificationQuickAction::MoveToArchive { remote_id } => {
+            move_msg(ctx, SystemLabel::Archive, remote_id).await?;
+        }
+        PushNotificationQuickAction::MoveToTrash { remote_id } => {
+            move_msg(ctx, SystemLabel::Trash, remote_id).await?;
+        }
     }
 
     Ok(())
 }
 
 #[instrument(skip_all)]
-async fn act(api: &Session, action: &PushNotificationQuickAction) -> MailContextResult<()> {
-    info!("Acting on message");
-
-    match action {
-        PushNotificationQuickAction::MarkAsRead { remote_id } => {
-            api.put_messages_read(vec![remote_id.clone()]).await?;
-        }
-
-        PushNotificationQuickAction::MoveToArchive { remote_id } => {
-            api.put_messages_label(vec![remote_id.clone()], LabelId::archive(), None)
+async fn read_msg(ctx: &MailUserContext, msg_id: MessageId) -> MailContextResult<()> {
+    match Message::find_or_fetch_by_remote_id(ctx, msg_id.clone()).await {
+        Ok(local_id) => {
+            ctx.queue_action(Read::new(std::iter::once(local_id)))
                 .await?;
         }
-
-        PushNotificationQuickAction::MoveToTrash { remote_id } => {
-            api.put_messages_label(vec![remote_id.clone()], LabelId::trash(), None)
+        Err(e) => {
+            warn!("Failed to resolve remote id, queuing fallback operation: {e}");
+            ctx.action_queue()
+                .queue_action(PushNotificationAction {
+                    action: PushNotificationQuickAction::MarkAsRead { remote_id: msg_id },
+                })
                 .await?;
         }
     }
+    Ok(())
+}
 
+#[instrument(skip_all)]
+async fn move_msg(
+    ctx: &MailUserContext,
+    label: SystemLabel,
+    msg_id: MessageId,
+) -> MailContextResult<()> {
+    match Message::find_or_fetch_by_remote_id(ctx, msg_id.clone()).await {
+        Ok(local_id) => {
+            let tether = ctx.user_stash().connection().await?;
+
+            // The likelihood of this failing is extremely low since system labels are
+            // pre-created ahead of time.
+            let label_id = label
+                .local_id(&tether)
+                .await?
+                .ok_or_else(|| LabelError::CouldNotResolveLocalLabel(label.remote_id()))?;
+
+            if let Some(action) = ActionMoveData::new(&tether, label_id, [local_id]).await? {
+                ctx.queue_action(Move(action)).await?;
+            }
+        }
+        Err(e) => {
+            warn!("Failed to resolve remote id, queuing fallback operation: {e}");
+            if label == SystemLabel::Archive {
+                ctx.action_queue()
+                    .queue_action(PushNotificationAction {
+                        action: PushNotificationQuickAction::MoveToArchive { remote_id: msg_id },
+                    })
+                    .await?;
+            } else if label == SystemLabel::Trash {
+                ctx.action_queue()
+                    .queue_action(PushNotificationAction {
+                        action: PushNotificationQuickAction::MoveToTrash { remote_id: msg_id },
+                    })
+                    .await?;
+            } else {
+                warn!("Received invalid system label: {label}");
+                return Ok(());
+            }
+        }
+    }
     Ok(())
 }
 
@@ -105,6 +153,26 @@ impl Handler for PushNotificationActionHandler {
         action: &mut Self::Action,
         _: WriterGuard<'_>,
     ) -> Result<(), MailContextError> {
-        act(&self.api, &action.action).await
+        match &action.action {
+            PushNotificationQuickAction::MarkAsRead { remote_id } => {
+                tracing::info!("Marking {remote_id:?} as read from push notification quick action");
+                self.api.put_messages_read(vec![remote_id.clone()]).await?;
+            }
+            PushNotificationQuickAction::MoveToArchive { remote_id } => {
+                tracing::info!(
+                    "Moving {remote_id:?} to Archive from push notification quick action"
+                );
+                self.api
+                    .put_messages_label(vec![remote_id.clone()], LabelId::archive(), None)
+                    .await?;
+            }
+            PushNotificationQuickAction::MoveToTrash { remote_id } => {
+                tracing::info!("Moving {remote_id:?} to Trash from push notification quick action");
+                self.api
+                    .put_messages_label(vec![remote_id.clone()], LabelId::trash(), None)
+                    .await?;
+            }
+        }
+        Ok(())
     }
 }
