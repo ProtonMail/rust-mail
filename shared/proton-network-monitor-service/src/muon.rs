48400d6cd99bb06b9978c1fa2b9581d1933ffb11
Pawel Rynowiecki <pawel.rynowiecki@proton.ch>
2025-11-04 18:26:03 +0100

    Revert "fix(ET-5276): Exclude request timeout from network failures"
    This reverts commit 6de4073527f82ec77506f627f44cae175ac9249a.

--- shared/proton-network-monitor-service/src/muon.rs
+++ shared/proton-network-monitor-service/src/muon.rs
@@ -1,7 +1,6 @@
 use crate::{ConnectionMonitor, RequestNetworkStatus};
-use muon::common::{BoxFut, Sender, SenderLayer, Timeout};
+use muon::common::{BoxFut, Sender, SenderLayer};
 use muon::{ProtonRequest, ProtonResponse};
-use std::error::Error;
 
 impl ConnectionMonitor {
     async fn on_send<S>(&self, inner: &S, req: ProtonRequest) -> muon::Result<ProtonResponse>
@@ -29,18 +28,10 @@ impl ConnectionMonitor {
         use muon::error::ErrorKind;
 
         match error.kind() {
-            ErrorKind::Tls | ErrorKind::Resolve | ErrorKind::Dial => {
+            ErrorKind::Tls | ErrorKind::Resolve | ErrorKind::Dial | ErrorKind::Send => {
                 self.update_request_status(RequestNetworkStatus::Offline);
             }
-            ErrorKind::Send => {
-                // We want to ignore muon's built in max time limit from the network detection
-                // logic since this can also be caused by long server response or slow network. This
-                // in turn does not mean that there is no network.
-                #[allow(clippy::redundant_closure_for_method_calls)] // false positive
-                if !error.source().is_some_and(|s| s.is::<Timeout>()) {
-                    self.update_request_status(RequestNetworkStatus::Offline);
-                }
-            }
+
             ErrorKind::Connect => {
                 self.update_request_status(RequestNetworkStatus::ServerUnreachable);
             }
