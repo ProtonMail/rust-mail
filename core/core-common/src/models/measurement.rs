58ce8de00e0d9a284c0f1a89c3e86ae675a3e196
Wojciech Polak <wojciech.polak@external.proton.ch>
2026-02-12 14:17:40 +0100

    chore(ET-5849): Delete after successful push

--- core/core-common/src/models/measurement.rs
+++ core/core-common/src/models/measurement.rs
@@ -3,18 +3,17 @@ use stash::{
     macros::Model,
     orm::Model,
     params,
-    stash::{Bond, StashError},
-    utils::{IterMapToSql as _, placeholders},
+    stash::{StashError, Tether},
 };
 
-use crate::datatypes::{MeasurementData, UnixTimestamp};
+use crate::datatypes::{LocalMeasurementId, MeasurementData, UnixTimestamp};
 
 #[derive(Debug, Clone, PartialEq, Model)]
 #[TableName("measurements")]
 #[Database(AccountDb)]
 pub struct Measurement {
     #[IdField(autoincrement)]
-    pub local_id: Option<i64>,
+    pub local_id: Option<LocalMeasurementId>,
 
     // We store everything in one field using JSON for two reasons:
     // 1. We don't really care about the content. We have no desire to index,
@@ -29,29 +28,13 @@ pub struct Measurement {
 }
 
 impl Measurement {
-    pub async fn take_batch(
+    pub async fn fetch_batch(
         limit: usize,
-        tx: &Bond<'_, AccountDb>,
+        tx: &Tether<AccountDb>,
     ) -> Result<Vec<Self>, StashError> {
         let measurements =
             Self::find("ORDER BY created_at ASC LIMIT ?", params![limit], tx).await?;
 
-        if !measurements.is_empty() {
-            let ids = measurements.iter().map(|i| i.local_id).bridge_sql();
-            let placeholders = placeholders(&ids);
-
-            tx.execute(
-                format!("DELETE FROM measurements WHERE local_id IN ({placeholders})"),
-                ids,
-            )
-            .await?;
-        }
-
         Ok(measurements)
-    }
-
-    pub async fn clear_all(tx: &Bond<'_, AccountDb>) -> Result<(), StashError> {
-        tx.execute("DELETE FROM measurements", params![]).await?;
-        Ok(())
     }
 }
